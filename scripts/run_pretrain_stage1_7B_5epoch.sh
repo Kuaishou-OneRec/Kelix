1417132fd5eb02b4f984509bdd053137779989a7
zhouyang12 <zhouyang12@kuaishou.com>
2024-12-24 11:13:36 +0800

    merge dataset

--- scripts/run_pretrain_stage1_7B_5epoch.sh
+++ scripts/run_pretrain_stage1_7B_5epoch.sh
@@ -1,7 +1,7 @@
 sed 's/=1/=8/g' /etc/mpi/hostfile  | head -1000 > /etc/mpi/hostfile_seq
 
 MODEL_DIR=/llm_reco_ssd/zhouyang12/models/Qwen2-7B-Instruct-DFN5B-ViT-H-14 # Pretrained model path
-OUTPUT_DIR=/llm_reco_ssd/zhouyang12/models/Qwen2-VL-7B-Instruct-LLaVA-CC3M-Pretrain-595K-bsz16x8
+OUTPUT_DIR=/llm_reco_ssd/zhouyang12/models/Qwen2-VL-7B-Instruct-LLaVA-CC3M-Pretrain-595K-bsz96x8-5epoch
 
 nnode=$(wc -l < /etc/mpi/hostfile_seq)
 
@@ -14,7 +14,7 @@ deepspeed --hostfile=/etc/mpi/hostfile_seq --num_nodes=$nnode \
     --batch_size 8 \
     --max_length 1536 \
     --save_checkpoint_every_epoch \
-    --num_epochs 1 \
+    --num_epochs 5 \
     --logging_per_step 1 \
     --merge_checkpoint \
     --merge_checkpoint_dtype bf16 \
