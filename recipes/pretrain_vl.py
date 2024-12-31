c3ccc683026e3880cf303e5d4a04f62a0ff2229c
zhouyang12 <zhouyang12@kuaishou.com>
2024-12-31 18:17:04 +0800

    debug blended_dataset

--- recipes/pretrain_vl.py
+++ recipes/pretrain_vl.py
@@ -239,15 +239,15 @@ def train():
   datasets, weights = zip(*[
     (wids.ShardListDataset(source), 1.0) for source in args.dataset.split(",")])
 
-
-  # blend_ds = BlendedWebDataset(
-  #   source_datasets=datasets,
-  #   weights=weights,
-  #   num_workers=8,
-  #   rank=dist.get_rank(),
-  #   world_size=dist.get_world_size(),
-  #   random_seed=args.seed
-  # )
+  ### 只切换到blended
+  dataset = BlendedWebDataset(
+    source_datasets=datasets,
+    weights=weights,
+    num_workers=8,
+    rank=dist.get_rank(),
+    world_size=dist.get_world_size(),
+    random_seed=args.seed
+  )
 
   collator = ImageTextPackingCollator(
       processor = processor,
@@ -264,17 +264,55 @@ def train():
       max_retry = 10,
       multiple_of = 8
   )
-  sources = args.dataset.split(",")
-  dataloader = get_indexed_dataloader(
-    sources=sources,
-    processor=processor,
+
+  dataloader = DataLoader(
+    dataset=dataset,
     batch_size=args.packing_batch_size,
+    shuffle=False,
     num_workers=8,
-    shuffle=True,
-    max_length=1024,
-    rank=dist.get_rank(),
-    collator=collator
+    collate_fn=collator
   )
+  ### 只切换到blended
+
+  # dataloader = get_indexed_dataloader(
+  #   sources=sources,
+  #   processor=processor,
+  #   batch_size=args.packing_batch_size,
+  #   num_workers=8,
+  #   shuffle=True,
+  #   max_length=1024,
+  #   rank=dist.get_rank(),
+  #   collator=collator
+  # )
+
+  #### 和master一样
+  # collator = ImageTextPackingCollator(
+  #     processor = processor,
+  #     max_length = args.max_length,
+  #     min_visual_tokens = 1,
+  #     max_visual_tokens = 1024,
+  #     max_text_length = 500,
+  #     spatial_merge_size = 2,
+  #     image_token_id = 151655,
+  #     video_token_id = 151656,
+  #     vision_start_token_id = 151652,
+  #     patch_size = 14,
+  #     shrink_ratio = 0.9,
+  #     max_retry = 10,
+  #     multiple_of = 8
+  # )
+  # sources = args.dataset.split(",")
+  # dataloader = get_indexed_dataloader(
+  #   sources=sources,
+  #   processor=processor,
+  #   batch_size=args.packing_batch_size,
+  #   num_workers=8,
+  #   shuffle=True,
+  #   max_length=1024,
+  #   rank=dist.get_rank(),
+  #   collator=collator
+  # )
+  #### 和master一样
 
   # dataset = ImageTextPairDatasetWithPacking(
   #     dataset = blend_ds,
