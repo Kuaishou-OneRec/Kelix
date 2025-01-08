005f82b00df67d108ef0e34a7d8c4f3b28b6a375
root <zhangzixing@kuaishou.com>
2025-01-08 21:09:16 +0800

    email=zhangzixing@kuaishou.com,time=20250108 21:09:16,script=examples/vlm/run_pretrain_stage2_7B.sh,node=52,comment=stage2，打开LLM训练，使用目前全部数据集，增加gradacc，mexlegth=8192，增加shuffle逻辑，增加数据集监控，lr恢复qwen，resume load ckpt,output=/llm_reco_ssd/luoxinchen/output/RecoVLM/Qwen2-VL-7B-stage2/0.0.15

--- examples/vlm/run_pretrain_stage2_7B.sh
+++ examples/vlm/run_pretrain_stage2_7B.sh
@@ -12,7 +12,7 @@ fi
 sed 's/=1/=8/g' /etc/mpi/hostfile  | head -1000 > /etc/mpi/hostfile_seq
 
 MODEL_DIR=/llm_reco_ssd/zhouyang12/models/Qwen2-7B-Instruct-DFN5B-ViT-H-14 # Pretrained/Base model path
-OUTPUT_DIR=/llm_reco_ssd/luoxinchen/output/RecoVLM/Qwen2-VL-7B-stage2/0.0.13
+OUTPUT_DIR=/llm_reco_ssd/luoxinchen/output/RecoVLM/Qwen2-VL-7B-stage2/0.0.15
 
 mkdir -p $OUTPUT_DIR
 
@@ -21,7 +21,7 @@ mkdir -p /tmp/_wids_cache
 nnode=$(wc -l < /etc/mpi/hostfile_seq)
 
 # 注意修改实验内容备注
-comment="stage2，打开LLM训练，使用目前全部数据集，增加gradacc，mexlegth=8192，增加shuffle逻辑，增加数据集监控，lr恢复qwen"
+comment="stage2，打开LLM训练，使用目前全部数据集，增加gradacc，mexlegth=8192，增加shuffle逻辑，增加数据集监控，lr恢复qwen，resume load ckpt"
 
 git add --all
 git commit -m "email=$email,time=$(date +"%Y%m%d %H:%M:%S"),script=$0,node=$nnode,comment=$comment,output=$OUTPUT_DIR"
