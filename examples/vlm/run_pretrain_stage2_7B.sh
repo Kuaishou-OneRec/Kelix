e79b5eae4728199b8d822e07f976d70eddfa0845
root <zhangzixing@kuaishou.com>
2025-01-08 22:37:37 +0800

    email=zhangzixing@kuaishou.com,time=20250108 22:37:37,script=examples/vlm/run_pretrain_stage2_7B.sh,node=52,comment=stage2，打开LLM训练，使用目前全部数据集，增加gradacc，mexlegth=8192，增加shuffle逻辑，增加数据集监控，lr恢复qwen，resume load ckpt, 打开ACC，stop step,output=/llm_reco_ssd/luoxinchen/output/RecoVLM/Qwen2-VL-7B-stage2/0.0.16.2

--- examples/vlm/run_pretrain_stage2_7B.sh
+++ examples/vlm/run_pretrain_stage2_7B.sh
@@ -12,7 +12,7 @@ fi
 sed 's/=1/=8/g' /etc/mpi/hostfile  | head -1000 > /etc/mpi/hostfile_seq
 
 MODEL_DIR=/llm_reco_ssd/zhouyang12/models/Qwen2-7B-Instruct-DFN5B-ViT-H-14 # Pretrained/Base model path
-OUTPUT_DIR=/llm_reco_ssd/luoxinchen/output/RecoVLM/Qwen2-VL-7B-stage2/0.0.16.1
+OUTPUT_DIR=/llm_reco_ssd/luoxinchen/output/RecoVLM/Qwen2-VL-7B-stage2/0.0.16.2
 
 mkdir -p $OUTPUT_DIR
 
@@ -21,7 +21,7 @@ mkdir -p /tmp/_wids_cache
 nnode=$(wc -l < /etc/mpi/hostfile_seq)
 
 # 注意修改实验内容备注
-comment="stage2，打开LLM训练，使用目前全部数据集，增加gradacc，mexlegth=8192，增加shuffle逻辑，增加数据集监控，lr恢复qwen，resume load ckpt, 关闭ACC"
+comment="stage2，打开LLM训练，使用目前全部数据集，增加gradacc，mexlegth=8192，增加shuffle逻辑，增加数据集监控，lr恢复qwen，resume load ckpt, 打开ACC，stop step"
 
 git add --all
 git commit -m "email=$email,time=$(date +"%Y%m%d %H:%M:%S"),script=$0,node=$nnode,comment=$comment,output=$OUTPUT_DIR"
@@ -57,4 +57,4 @@ nohup deepspeed --hostfile=/etc/mpi/hostfile_seq --num_nodes=$nnode \
     --merge_checkpoint_output_file pytorch_model.bin \
     --comment "$comment" \
     --commit_id $git_hash \
-    --deepspeed --deepspeed_config examples/vlm/configs/ds_z2_config_7B.json >> $OUTPUT_DIR/stdout.log 2>>$OUTPUT_DIR/stderr.log &
+    --deepspeed --deepspeed_config examples/vlm/configs/ds_z2_config_7B_grad_acc.json >> $OUTPUT_DIR/stdout.log 2>>$OUTPUT_DIR/stderr.log &
--- recipes/pretrain_vl.py
+++ recipes/pretrain_vl.py
@@ -229,7 +229,8 @@ def train():
     optimizer=optimizer,
     num_warmup_steps=args.num_warmup_steps,
     num_training_steps=args.num_training_steps,
-    min_lr=args.min_lr
+    min_lr=args.min_lr,
+    num_stop_steps=10
   )
 
   model.train()
