d022e75d3b9bd592e4625761e2d60aab962783ec
root <zhouyang12@kuaishou.com>
2025-01-11 20:24:11 +0800

    email=zhouyang12@kuaishou.com,time=20250111 20:24:11,script=examples/vlm/run_pretrain_stage2_7B.sh,node=10,comment=测试stage2，打开LLM训练，使用the_cauldron，打开gradient\ checkpoint,\ max_length=16000,测试sequence_parallel,使用ulysses,测试32k,测试不同部分的性能,output=/llm_reco_ssd/luoxinchen/output/RecoVLM/Qwen2-VL-7B-stage2/0.0.20.1

--- examples/vlm/run_pretrain_stage2_7B.sh
+++ examples/vlm/run_pretrain_stage2_7B.sh
@@ -36,7 +36,7 @@ export PYTHONPATH=$PWD:$PYTHONPATH
 nohup deepspeed --hostfile=/etc/mpi/hostfile_seq --num_nodes=$nnode \
     recipes/pretrain_vl.py --model_dir $MODEL_DIR \
     --output_dir $OUTPUT_DIR \
-    --dataset_config /llm_reco_ssd/zhouyang12/code/RecoVLM/examples/vlm/configs/the_cauldron_cot.json \
+    --dataset_config examples/vlm/configs/the_cauldron_cot.json \
     --resume_from /llm_reco_ssd/luoxinchen/output/RecoVLM/Qwen2-VL-7B-stage1-v0.0.36 \
     --resume_from_tag global_step90000 \
     --enable_gradient_checkpointing \
