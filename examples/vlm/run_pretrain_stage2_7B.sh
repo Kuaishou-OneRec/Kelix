ddc8b1a5071590c32f60dc11054cb6d40e35b70d
root <zhangzixing@kuaishou.com>
2025-01-12 22:20:40 +0800

    email=zhangzixing@kuaishou.com,time=20250112 22:20:40,script=examples/vlm/run_pretrain_stage2_7B.sh,node=50,comment=stage2，打开LLM训练，使用目前全部数据集，max_len=8192，compile=True，增加数据集，add padding，add eos token, add image pad,output=/llm_reco_ssd/luoxinchen/output/RecoVLM/Qwen2-VL-7B-stage2/0.0.22

--- examples/vlm/run_pretrain_stage2_7B.sh
+++ examples/vlm/run_pretrain_stage2_7B.sh
@@ -59,4 +59,4 @@ nohup deepspeed --hostfile=/etc/mpi/hostfile_seq --num_nodes=$nnode \
     --monitor_datasource_cnt \
     --comment "$comment" \
     --commit_id $git_hash \
-    --deepspeed --deepspeed_config examples/vlm/configs/ds_z2_config_7B.json >> $OUTPUT_DIR/stdout.log 2>>$OUTPUT_DIR/stderr.log &
+    --deepspeed --deepspeed_config examples/vlm/configs/ds_z1_config_7B.json >> $OUTPUT_DIR/stdout.log 2>>$OUTPUT_DIR/stderr.log &
