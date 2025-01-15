de2a2f6fc9a91ffe5ae35cec5ae8baaf02cd04c3
root <zhouyang12@kuaishou.com>
2025-01-16 00:37:35 +0800

    email=zhouyang12@kuaishou.com,time=20250116 00:37:35,script=examples/vlm/run_pretrain_stage1_7B.sh,node=32,comment=一阶段训练,20250115,frompretrained,output=/llm_reco_ssd/luoxinchen/output2/RecoVLM/Qwen2-VL-7B-stage1/0.0.38

--- recipes/pretrain_vl.py
+++ recipes/pretrain_vl.py
@@ -188,6 +188,8 @@ def train():
   if dist.get_rank() == 0:
     import subprocess
 
+    print_rank_0(os.environ["HADOOP_HOME"])
+
     command = f'HADOOP_CLIENT_OPTS="-Xmx4g" hdfs dfs -ls viewfs://hadoop-lt-cluster/home/reco_wl/mpi/luoxinchen/recovlm_stage1_shuffle_20250115'
     result = subprocess.run(command, shell=True, check=True, capture_output=True, text=True)
     print(result)
