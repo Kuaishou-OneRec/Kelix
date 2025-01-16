800bdbff3cfb3542ba17fe3097c2b779dffcf029
zhouyang12 <zhouyang12@kuaishou.com>
2025-01-16 10:56:36 +0800

    rm scripts

--- recovlm/data/datasets.py
+++ recovlm/data/datasets.py
@@ -1395,10 +1395,8 @@ class ChatCompletionVisionParquetDataset(ChatCompletionVisionDataset):
           data_files = [fn for fn in data_files if fn.endswith(".parquet")]
       elif isinstance(sources, list):
         for source in sources:
-          with open(source) as f:
-            data_files.extend(f.read().split("\n"))
-          # hdfs_files = shell_hdfs_ls(source)
-          # data_files += [fn for fn in hdfs_files if fn.endswith(".parquet")]
+          hdfs_files = shell_hdfs_ls(source)
+          data_files += [fn for fn in hdfs_files if fn.endswith(".parquet")]
 
     data_files.sort()
     self.rng.shuffle(data_files)
