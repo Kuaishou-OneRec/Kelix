1eb2278c1c7ec75d5e0402d4e955a373567cd484
root <zhouyang12@kuaishou.com>
2025-02-24 19:08:41 +0800

    email=zhouyang12@kuaishou.com,time=20250224 19:08:41,script=examples/vlm/run_pretrain_stage2_72B_fsdp.sh,node=16,comment=stage2，stg1 vit load qwen，使用ocr 进行stg2,output=/llm_reco_ssd/luoxinchen/output2/RecoVLM-dev/Qwen2-VL-7B-stage2/0.0.40

--- recipes/train_fsdp.py
+++ recipes/train_fsdp.py
@@ -384,6 +384,20 @@ def train():
     model = Qwen2VLForConditionalGeneration.from_pretrained(
       args.model_dir, _attn_implementation="flash_attention_2",
       use_cache=False)
+  if dist.get_rank() == 0:
+    tensor = torch.zeros(
+      (128, 32),
+      device="cuda",
+      dtype=torch.bfloat16
+    )
+  else:
+    tensor = torch.empty(
+      (128, 32),
+      device="cuda",
+      dtype=torch.bfloat16
+    )
+  dist.broadcast(tensor, src=0)
+  print(f"{tensor.shape}, {tensor.dtype}")
   #model.train()
   
   for param in model.parameters():
