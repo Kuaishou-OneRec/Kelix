b7909aae439d61d9b88c89cc0597a92eb9a5d471
root <zhouyang12@kuaishou.com>
2025-02-24 16:42:53 +0800

    email=zhouyang12@kuaishou.com,time=20250224 16:42:53,script=./examples/vlm/run_pretrain_stage2_72B_fsdp.sh,node=16,comment=stage2，stg1 vit load qwen，使用ocr 进行stg2,output=/llm_reco_ssd/luoxinchen/output2/RecoVLM/Qwen2-VL-7B-stage2/0.0.34

--- recovlm/training/distributed.py
+++ recovlm/training/distributed.py
@@ -216,6 +216,7 @@ def load_from_full_model_state_dict(
             "Sharded State Dict doesn't equal to Full State Dict"
         print(" - ", set(meta_sharded_sd.keys()) - set(full_sd.keys()))
         print(" + ", set(full_sd.keys()) - set(meta_sharded_sd.keys()))
+        print(meta_sharded_sd.keys(), full_sd.keys())
         assert list(meta_sharded_sd.keys()) == list(full_sd.keys()), \
             "Keys of Sharded State Dict doesn't equal to Full State Dict"
         for (param_name, full_param), sharded_meta_param in zip(
