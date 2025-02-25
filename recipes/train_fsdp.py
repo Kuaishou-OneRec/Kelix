d6085a73769738768239b100d8f022de0df8fcb5
root <zhouyang12@kuaishou.com>
2025-02-25 11:13:28 +0800

    email=zhouyang12@kuaishou.com,time=20250225 11:13:27,script=examples/vlm/run_pretrain_stage2_72B_fsdp.sh,node=16,comment=stage2，stg1 vit load qwen，使用ocr 进行stg2,output=/llm_reco_ssd/luoxinchen/output2/RecoVLM-dev/Qwen2-VL-7B-stage2/0.0.40

--- recipes/train_fsdp.py
+++ recipes/train_fsdp.py
@@ -409,9 +409,12 @@ def train():
     model=model,
     shard_conditions=[get_shard_conditions],
     cpu_offload=False,
-    reshard_after_forward=True,
+    reshard_after_forward=False,
     dp_mesh=device_mesh,
   )
+  print_rank_0(model)
+  import time
+  time.sleep(30)
   dist.barrier()
 
   with Timer("Load state dict"):
