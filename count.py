2ab024d084519789099425edc759849831624bac
maosiyang <you@kuaishou.com>
2025-03-07 18:04:33 +0800

    sd

--- count.py
+++ count.py
@@ -2,7 +2,7 @@ import pandas as pd
 import json
 
 # 读取Parquet文件
-file_path = '/llm_reco_ssd/maosiyang/wenjuan_benchmark_dataset/rank-0-c089702c-faa0-11ef-a168-a400e2727258.parquet'
+file_path = '/llm_reco/maosiyang/dataset/wenjuan/rank-0-5db9c286-fa6d-11ef-be0f-a400e2727258.parquet'
 df = pd.read_parquet(file_path)
 
 # 统计assistant的回答中包含“和源视频最相似的视频是【视频2】”的数量
@@ -12,7 +12,7 @@ for row in df['messages']:
     for message in messages:
         if message['role'] == 'assistant':
             for content in message['content']:
-                if content['type'] == 'text' and "和源视频最相似的视频是【视频2】" in content['text']:
+                if content['type'] == 'text' and "【结果：不满意】" in content['text']:
                     count += 1
 
-print(f"和源视频最相似的视频是【视频2】的回答数量: {count}")
+print(f"【结果：不满意】的回答数量: {count}") 
