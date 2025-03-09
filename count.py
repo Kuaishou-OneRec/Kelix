f7520ddac5a67cc26efd201d8ffc96f318b8d108
maosiyang <you@kuaishou.com>
2025-03-09 15:33:17 +0800

    repair bug

--- count.py
+++ count.py
@@ -2,11 +2,12 @@ import pandas as pd
 import json
 
 # 读取Parquet文件
-file_path = '/llm_reco/maosiyang/dataset/wenjuan/rank-0-5db9c286-fa6d-11ef-be0f-a400e2727258.parquet'
+file_path = '/llm_reco/maosiyang/dataset/i2i/rank-0-dad122ea-fcb7-11ef-a51a-a400e2727258.parquet'
 df = pd.read_parquet(file_path)
 
 # 统计assistant的回答中包含“和源视频最相似的视频是【视频2】”的数量
 count = 0
+allcount = 0
 for row in df['messages']:
     messages = json.loads(row)
     for message in messages:
@@ -14,5 +15,7 @@ for row in df['messages']:
             for content in message['content']:
                 if content['type'] == 'text' and "【结果：不满意】" in content['text']:
                     count += 1
-
+                allcount += 1
 print(f"【结果：不满意】的回答数量: {count}") 
+print(f"总回答数量: {allcount}")
+print(f"【结果：满意】的回答数量: {allcount - count}")
