8697bb5d499a1742e68a4e74c1634a6cc93f5960
凌致新 <lingzhixin@kuaishou.com>
2025-04-01 17:43:01 +0800

    c

---
+++ killall.sh
@@ -0,0 +1,2 @@
+mpirun --allow-run-as-root --hostfile /etc/mpi/hostfile --pernode bash -c "pkill -9 python3"
+mpirun --allow-run-as-root --hostfile /etc/mpi/hostfile --pernode bash -c "pkill -9 pt_main_thread" 
