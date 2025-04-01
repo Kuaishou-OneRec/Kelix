0cfe899a866ae38cbdec1d1087f87cc8393466c9
凌致新 <lingzhixin@kuaishou.com>
2025-04-01 17:51:51 +0800

    c

--- recipes/train_fsdp.py
+++ recipes/train_fsdp.py
@@ -185,7 +185,7 @@ def get_argument_parser():
   parser.add_argument("--enable_gradient_checkpointing", action="store_true",
                       help="Enable gradient checkpointing during training")
   
-  parser.add_argument("--gradient_accumulation_steps", type=int, default=4,
+  parser.add_argument("--gradient_accumulation_steps", type=int, default=1,
                       help="Gradient accumulation steps")
   
   parser.add_argument("--sequence_parallel_size", type=int, default=1,
