0bbbd772c638a5886fba91d0469b42ff6027d98d
penghao03 <penghao03@kuaishou.com>
2025-04-11 23:12:15 +0800

    email=penghao03@kuaishou.com,time=20250411 23:12:15,script=./examples/vlm/run_sft_7B_fsdp_intern_ph.sh,node=2,comment=Intern-vl debug,output=/llm_reco/penghao03/intern-vl/output

--- recipes/train_fsdp_debug.py
+++ recipes/train_fsdp_debug.py
@@ -426,7 +426,8 @@ def train():
                 args.model_dir, _attn_implementation="flash_attention_2",
                 use_cache=False
       )
-  print(model._tp_plan)
+  print_rank_0(model._tp_plan)
+  print_rank_0("*****--sfaf"*100)
   # check all param & buffer on meta device
   for tensor in itertools.chain(model.parameters(), model.buffers()):
     assert tensor.device == torch.device("meta")
