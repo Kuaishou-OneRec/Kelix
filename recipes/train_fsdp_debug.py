9b0c4bea4605cc2dcc792e90e3f9198b36e97313
penghao03 <penghao03@kuaishou.com>
2025-04-11 22:28:40 +0800

    email=penghao03@kuaishou.com,time=20250411 22:28:40,script=./examples/vlm/run_sft_7B_fsdp_intern_ph.sh,node=2,comment=Intern-vl debug,output=/llm_reco/penghao03/intern-vl/output

--- recipes/train_fsdp_debug.py
+++ recipes/train_fsdp_debug.py
@@ -15,7 +15,7 @@ import itertools
 
 os.system("pip install timm")
 os.system("pip install accelerate>=0.26.0")
-os.system("pip install transformers==4.51.0")
+os.system("pip install transformers==4.51.2")
 
 from recovlm.training.checkpoint import AppState, DistributedCheckpointer
 
