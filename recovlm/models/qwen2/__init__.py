d49755f3426694686fd531f8273c4883483ad8bb
penghao03 <penghao03@kuaishou.com>
2025-04-12 12:16:07 +0800

    email=penghao03@kuaishou.com,time=20250412 12:16:07,script=./examples/vlm/run_sft_7B_fsdp_intern_ph.sh,node=2,comment=Intern-vl debug,output=/llm_reco/penghao03/intern-vl/output

--- recovlm/models/qwen2/__init__.py
+++ recovlm/models/qwen2/__init__.py
@@ -1,4 +1,4 @@
 from .modeling_qwen2 import Qwen2ForCausalLM
-from .configuration_qwen2 import Qwen2Config
+from configuration_qwen import Qwen2Config
 
 __all__ = ['Qwen2ForCausalLM', 'Qwen2Config']
