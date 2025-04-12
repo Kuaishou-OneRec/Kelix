30d4d2ac7f8235cb0a29ffe392ec2f0279b16ff4
penghao03 <penghao03@kuaishou.com>
2025-04-12 10:01:44 +0800

    email=penghao03@kuaishou.com,time=20250412 10:01:44,script=./examples/vlm/run_sft_7B_fsdp_intern_ph.sh,node=2,comment=Intern-vl debug,output=/llm_reco/penghao03/intern-vl/output

--- recovlm/models/intern_vl_3/__init__.py
+++ recovlm/models/intern_vl_3/__init__.py
@@ -1,7 +1,7 @@
 from .configuration_intern_vit import InternVisionConfig
 from .configuration_internvl_chat import InternVLChatConfig
 from .modeling_intern_vit import InternVisionModel
-from .modeling_internvl_chat import InternVLChatModel
+from .modeling_internvl_chat import InternVLChatModel,split_model
 
 __all__ = ['InternVisionConfig', 'InternVisionModel',
            'InternVLChatConfig', 'InternVLChatModel','split_model']
