f35302138dec53f9f192120c740a3572e8039ae4
penghao03 <penghao03@kuaishou.com>
2025-04-13 22:52:10 +0800

    email=penghao03@kuaishou.com,time=20250413 22:52:09,script=./examples/vlm/run_pretrain_stage1_7B.sh,node=2,comment=debug,output=/llm_reco/penghao03/intern-vl/output

--- recipes/pretrain_vl_intern_vl.py
+++ recipes/pretrain_vl_intern_vl.py
@@ -26,7 +26,7 @@ from recovlm.models.qwen2_vl.processing_qwen2_vl import Qwen2VLProcessor
 from recovlm.models.qwen2_vl import Qwen2VLForConditionalGeneration
 from recovlm.models.intern_vl_3 import InternVLChatModel
 from recovlm.data.dataloaders_v2 import get_dataloader as get_dataloader_v2
-from recovlm.data.dataloaders import get_dataloader
+#from recovlm.data.dataloaders import get_dataloader
 
 from recovlm.utils.merge_checkpoints import convert_zero_checkpoint_to_state_dict
 from recovlm.losses import CrossEntropyLoss, CrossEntropyLossReweight
