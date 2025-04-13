a8f25abe4c61e8186e18ee75e0a2c4a6f5649768
penghao03 <penghao03@kuaishou.com>
2025-04-13 12:24:31 +0800

    email=penghao03@kuaishou.com,time=20250413 12:24:31,script=./examples/vlm/run_pretrain_stage1_7B.sh,node=2,comment=debug,output=/llm_reco/penghao03/intern-vl/output

--- examples/vlm/run_pretrain_stage1_7B.sh
+++ examples/vlm/run_pretrain_stage1_7B.sh
@@ -13,7 +13,7 @@ sed 's/=1/=8/g' /etc/mpi/hostfile  | head -1000 > /etc/mpi/hostfile_seq
 
 # MODEL_DIR=/llm_reco_ssd/zhouyang12/models/Qwen2-7B-DFN5B-ViT-H-14 # Pretrained model path
 MODEL_DIR=/llm_reco_ssd/zhouyang12/models/Qwen2-7B-Instruct-DFN5B-ViT-H-14 # Pretrained model path
-OUTPUT_DIR=/llm_reco_ssd/luoxinchen/output2/RecoVLM/Qwen2-VL-7B-stage1/0.0.41 # 
+#OUTPUT_DIR=/llm_reco_ssd/luoxinchen/output2/RecoVLM/Qwen2-VL-7B-stage1/0.0.41 # 
 OUTPUT_DIR=/llm_reco/penghao03/intern-vl/output
 mkdir -p $OUTPUT_DIR
 
@@ -21,7 +21,7 @@ mkdir -p /tmp/_wids_cache
 
 nnode=$(wc -l < /etc/mpi/hostfile_seq)
 
-comment="一阶段训练,20250208，instruct model, ocr"
+comment="debug"
 
 git add --all
 git commit -m "email=$email,time=$(date +"%Y%m%d %H:%M:%S"),script=$0,node=$nnode,comment=$comment,output=$OUTPUT_DIR"
