9aac62635e5c71b6dbcffbd7a0a158074db33ee0
penghao03 <penghao03@kuaishou.com>
2025-04-15 00:55:23 +0800

    email=penghao03@kuaishou.com,time=20250415 00:55:23,script=examples/vlm/run_pretrain_stage1_7B_ph.sh,node=2,comment=debug,output=/llm_reco/penghao03/intern-vl/output/pretrain_dataset

--- recovlm/utils/intern_vl_utils.py
+++ recovlm/utils/intern_vl_utils.py
@@ -5,14 +5,12 @@ from collections import Counter
 from typing import Dict
 
 import cv2
-import imageio
 import numpy as np
 import torch
 import torch.nn.functional as F
 import torchvision.transforms as T
 import transformers
 from decord import VideoReader
-from internvl.conversation import get_conv_template
 from PIL import Image
 from torch.utils.data import ConcatDataset, WeightedRandomSampler
 from torchvision.transforms.functional import InterpolationMode
