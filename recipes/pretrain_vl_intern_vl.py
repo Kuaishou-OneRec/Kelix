424bf4cfd6a9bae13c821d353f59849871101b88
penghao03 <penghao03@kuaishou.com>
2025-04-14 17:13:55 +0800

    email=penghao03@kuaishou.com,time=20250414 17:13:55,script=examples/vlm/run_pretrain_stage1_7B_ph.sh,node=2,comment=debug,output=/llm_reco/penghao03/intern-vl/output/pretrain_dataset

--- recipes/pretrain_vl_intern_vl.py
+++ recipes/pretrain_vl_intern_vl.py
@@ -443,11 +443,7 @@ def train():
     dataset_config = json.loads(f.read())
   dataset = dataset_config.pop("name")
   dataset_config['model_type']=args.model_type
-  if model_type=="intern-vl":
-    dataset_config['force_image_size'] = model.config.force_image_size
-    dataset_config['patch_size'] = model.vision_config.patch_size
-    dataset_config['num_image_token'] = int((model.config.force_image_size // model.vision_config.patch_size) ** 2 * (args.down_sample_ratio ** 2))
-
+  
   if args.max_length:
     print_rank_0(
       f"Overwrite max_length in dataset_config: "
