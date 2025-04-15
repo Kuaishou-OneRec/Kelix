4eecdf53dae7d936607e829a761de01fbc81bee3
penghao03 <penghao03@kuaishou.com>
2025-04-15 17:48:00 +0800

    email=penghao03@kuaishou.com,time=20250415 17:48:00,script=examples/vlm/run_pretrain_stage1_7B_ph.sh,node=2,comment=debug,output=/llm_reco/penghao03/intern-vl/output/pretrain_dataset

--- recovlm/data/datasets.py
+++ recovlm/data/datasets.py
@@ -2553,8 +2553,6 @@ class InternVLChatCompletionVisionDataset(IterableDataset):
     pixel_values = [self.transform(image) for image in images]
     pixel_values = torch.stack(pixel_values)
     inputs["pixel_values"] = pixel_values
-    print(inputs["input_ids"].shape)
-    print(self.tokenizer.batch_decode(inputs["input_ids"]))
 
     # For the Warning: (add by zzx)
     #   Token indices sequence length is longer than the specified maximum 
@@ -2582,7 +2580,7 @@ class InternVLChatCompletionVisionDataset(IterableDataset):
           f"Unable to generate sample with 0 loss_mask."
         )
     position_ids = inputs['attention_mask'].long().cumsum(-1) - 1
-    position_ids.masked_fill_(ret['attention_mask'] == 0, 1)
+    position_ids.masked_fill_(inputs['attention_mask'] == 0, 1)
     inputs["position_ids"] = position_ids
     inputs.pop("attention_mask")
     return inputs
