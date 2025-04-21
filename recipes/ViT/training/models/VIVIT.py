384d666280b23f2e488ac4c32143d89f27c9c879
maosiyang <you@kuaishou.com>
2025-04-21 11:20:18 +0800

    na

--- recipes/ViT/training/models/VIVIT.py
+++ recipes/ViT/training/models/VIVIT.py
@@ -6,8 +6,8 @@ from PIL import Image
 import torch.nn as nn
 import torch.distributed as dist
 from transformers import AutoProcessor, AutoModel
-from recipes.ViT.training.models.vivit.modeling_vivit import VivitPreTrainedModel, VivitModel
-from recipes.ViT.training.models.vivit.processing_vivit import VivitProcessor
+from recipes.ViT.training.models.vivit.image_processing_vivit import VivitImageProcessor
+from recipes.ViT.training.models.vivit.modeling_vivit import VivitModel
 import torch.nn.functional as F
 from recipes.ViT.training.models.siglip.modeling_siglip import SiglipPreTrainedModel, SiglipModel
 from recipes.ViT.training.models.siglip.processing_siglip import SiglipProcessor
