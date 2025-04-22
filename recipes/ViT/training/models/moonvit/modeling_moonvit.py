d7002cfc0f2cb31030df20eaa939bcdef01a6c1b
maosiyang <you@kuaishou.com>
2025-04-22 15:26:03 +0800

    na

--- recipes/ViT/training/models/moonvit/modeling_moonvit.py
+++ recipes/ViT/training/models/moonvit/modeling_moonvit.py
@@ -560,7 +560,7 @@ class MoonVitPretrainedModel(PreTrainedModel):
             merged_patch = self.proj(merged_patch).view(merged_patch.size(0), -1)
             merged_patch = merged_patch.mean(dim=0)
             # L2 normalize the embedding vector
-            merged_patch = F.normalize(merged_patch, p=2, dim=1)
+            merged_patch = F.normalize(merged_patch, p=2, dim=-1)
             merged_patches_list.append(merged_patch)
         image_embeddings = torch.stack(merged_patches_list)
         print(image_embeddings)
