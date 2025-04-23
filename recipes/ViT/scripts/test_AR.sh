aecc796390cdb47296063bb2dda7ea13b01f3af3
liuyang76 <liuyang76@kuaishou.com>
2025-04-23 20:52:30 +0800

    Add AR loss and support Kimi's ViT training.

--- recipes/ViT/scripts/test_AR.sh
+++ recipes/ViT/scripts/test_AR.sh
@@ -14,4 +14,4 @@ deepspeed --hostfile=/etc/mpi/hostfile_seq --num_nodes=$nnode \
     --config_file /llm_reco/liuyang76/codes/llm_rec_code_base/recovlm/recipes/ViT/configs/v1.yaml \
     --output_dir $OUTPUT_DIR \
     --deepspeed \
-    --deepspeed_config examples/vlm/configs/ds_z1_config_7B.json > $OUTPUT_DIR/stdout.log 2>$OUTPUT_DIR/stderr.log
+    --deepspeed_config examples/vlm/configs/ds_stage2.json > $OUTPUT_DIR/stdout.log 2>$OUTPUT_DIR/stderr.log
