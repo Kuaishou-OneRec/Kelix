0ce2edb0fef100faa732cdb6fd909f47d400df0a
maosiyang <you@kuaishou.com>
2025-04-24 14:54:26 +0800

    na

--- tools/data_helpers/converters/converter_vit.py
+++ tools/data_helpers/converters/converter_vit.py
@@ -113,7 +113,7 @@ def flush(fs, buffer, temp_dir, output_dir):
             fs.mv(tempfile, filename)
         except Exception as e:
             print(e)
-            print(os.path.exists(tempfile))
+            print('msy',tempfile)
     else:
         command = "mv {} {}".format(tempfile, filename)
         subprocess.run(command, shell=True, check=True)
