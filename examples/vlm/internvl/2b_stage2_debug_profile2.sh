cee383092571387736294f9bd1d60136a800850b
凌致新 <lingzhixin@kuaishou.com>
2025-04-25 15:54:19 +0800

    c

--- examples/vlm/internvl/2b_stage2_debug_profile2.sh
+++ examples/vlm/internvl/2b_stage2_debug_profile2.sh
@@ -136,3 +136,4 @@ nohup mpirun --allow-run-as-root -np $np \
                 --kml_id $KML_ID \
                 --kml_task_id $KML_TASK_ID \
                 --heartbeat_monitor > $OUTPUT_DIR/stdout.log 2>$OUTPUT_DIR/stderr.log &
+
