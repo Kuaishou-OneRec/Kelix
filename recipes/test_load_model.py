869c7431047172b7e0ea3698ce13910d1094fc95
maosiyang <you@kuaishou.com>
2025-04-27 20:50:14 +0800

    na

--- recipes/test_load_model.py
+++ recipes/test_load_model.py
@@ -1,9 +1,17 @@
 from recovlm.models.qwen_2_5_vl.modeling_qwen2_5_vl import Qwen2_5_VLForConditionalGeneration_moonvit
+from recovlm.models.qwen_2_5_vl.processing_qwen2_5_vl import Qwen2_5_VLProcessor_moonvit 
 model = Qwen2_5_VLForConditionalGeneration_moonvit.from_pretrained(
-  "/llm_reco_ssd/zhouyang12/models/Qwen2-VL-7B-Instruct"
+  "/llm_reco_ssd/zhouyang12/models/Qwen2-VL-7B-Instruct",allow_missing_keys=True
 )
 model.eval()
+processor = Qwen2_5_VLProcessor_moonvit.from_pretrained(
+  "/llm_reco_ssd/zhouyang12/models/Qwen2-VL-7B-Instruct"
+)
+
+
+
 images = torch.randn(1, 3, 224, 224)
 texts = ["hello world"]
-rets = model(images, texts)
+data = processor(images=images, text=texts)
+rets = model(data)
 print(rets)
