482c6aa9a354c264503c2b153f8429091d2f63fc
maosiyang <you@kuaishou.com>
2025-04-27 20:52:05 +0800

    na

--- recipes/test_load_model.py
+++ recipes/test_load_model.py
@@ -1,7 +1,7 @@
 from recovlm.models.qwen_2_5_vl.modeling_qwen2_5_vl import Qwen2_5_VLForConditionalGeneration_moonvit
 from recovlm.models.qwen_2_5_vl.processing_qwen2_5_vl import Qwen2_5_VLProcessor_moonvit 
 model = Qwen2_5_VLForConditionalGeneration_moonvit.from_pretrained(
-  "/llm_reco_ssd/zhouyang12/models/Qwen2-VL-7B-Instruct",allow_mismatched_sizes=True
+  "/llm_reco_ssd/zhouyang12/models/Qwen2-VL-7B-Instruct",ignore_mismatched_sizes=True
 )
 model.eval()
 processor = Qwen2_5_VLProcessor_moonvit.from_pretrained(
