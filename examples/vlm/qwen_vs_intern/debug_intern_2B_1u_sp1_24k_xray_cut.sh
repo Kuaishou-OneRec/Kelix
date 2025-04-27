29690edbdfa763180cc6e2349f4f04460b440b49
root <you@kuaishou.com>
2025-04-28 02:38:17 +0800

    email=you@kuaishou.com,time=20250428 02:38:17,script=examples/vlm/qwen_vs_intern/debug_intern_2B_1u_sp1_24k_xray_cut.sh,node=1,comment=run internvl 2b stage1 by ccl,output=/llm_reco/lingzhixin/output/internvl-2b/qwen_vs_intern/debug_intern_2B_1u_sp1_24k_xray_cut

--- examples/vlm/qwen_vs_intern/debug_intern_2B_1u_sp1_24k_xray_cut.sh
+++ examples/vlm/qwen_vs_intern/debug_intern_2B_1u_sp1_24k_xray_cut.sh
@@ -121,7 +121,7 @@ nohup mpirun --allow-run-as-root \
                 --monitor_datasource_cnt \
                 --dataset_config examples/vlm/configs/2b_internvl_stage2_cut.json \
                 --max_length 24000 \
-                --learning_rate 2e-4 \
+                --learning_rate 4e-7 \
                 --model_class InternVLChatModel \
                 --min_lr 0.0 \
                 --weight_decay 0.01 \
