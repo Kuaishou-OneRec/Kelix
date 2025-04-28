d6f08a907f881cee771660e25fabf93bfb8293a1
maosiyang <you@kuaishou.com>
2025-04-28 20:48:15 +0800

    na

--- tests/run_test_datasets.sh
+++ tests/run_test_datasets.sh
@@ -1,1 +1,1 @@
-nohup python3 /llm_reco/maosiyang/msy_internvl_premerge/recovlm/tests/test_datasets.py /llm_reco/maosiyang/msy_internvl_premerge/recovlm/tests/files.txt > /llm_reco/maosiyang/msy_internvl_premerge/recovlm/tests/std.log 2> /llm_reco/maosiyang/msy_internvl_premerge/recovlm/tests/err.log &
+python3 /llm_reco/maosiyang/msy_internvl_premerge/recovlm/tests/test_datasets.py /llm_reco/maosiyang/msy_internvl_premerge/recovlm/tests/files.txt
