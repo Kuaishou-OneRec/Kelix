aaae184c0c3a37492e3d4b1df40176bf32e13ff9
maosiyang <you@kuaishou.com>
2025-04-28 19:46:38 +0800

    na

--- tests/test_datasets copy.py
+++ tests/test_datasets copy.py
@@ -1,6 +1,6 @@
 import os
 import torch
-import sys
+
 import wids
 import logging
 import json
@@ -311,24 +311,22 @@ def gather_batches(buffer, group):
     return gathered_batches
 
 
-def test_InternVLParquetDataset(sources):
+def test_InternVLParquetDataset():
     init_processes(0, 1)
     from transformers import AutoTokenizer, AutoProcessor
     from recovlm.data.datasets import InternVLChatCompletionVisionParquetDataset
-    from recovlm.data.dataloaders import get_chat_completion_vision_parquet_dataloader
     processor = AutoProcessor.from_pretrained("/llm_reco_ssd/zhouyang12/models/InternVL3-2B", trust_remote_code=True)
     path = "/llm_reco/chuchenglong/work_space/recovlm/examples/vlm/configs/internvl/2b_internvl_stage2.json"
     with open(path, encoding="utf-8") as f:
         dataset_config = json.loads(f.read())
     dataset_config.pop("name")
-    dataset_config["num_workers"] = 100
+    dataset_config["num_workers"] = 1
     dataset_config["shuffle_seed"] = int(time.time())
-    dataset_config["max_length"] = 999999999
-    dataset_config["sources"] = sources
-    assert sources[0][:4] == 'view' and isinstance(sources, list)
+    dataset_config["max_length"] = 16000
+    dataset_config["sources"] = ["viewfs://hadoop-lt-cluster/home/reco_wl/mpi/chuchenglong/pt/0421/stage2_ccl_v3_0425/_prepared/0/prep-0-5f8467a5aa2c472d9c31bbb81356540f.parquet"]
     # viewfs://hadoop-lt-cluster/home/reco_wl/mpi/luoxinchen/recovlm_dataset_stage2/Wanjuan_reconstruct/rank-0-0098b494-d499-11ef-9d06-946daee91052.parquet
     # dataset_config["sources"] = ["viewfs://hadoop-lt-cluster/home/reco_wl/mpi/luoxinchen/recovlm_dataset_stage2/Wanjuan_reconstruct/rank-0-0098b494-d499-11ef-9d06-946daee91052.parquet"]
-    print(3333, sources)
+
     dataset = InternVLChatCompletionVisionParquetDataset(cut_to_pad=True, **dataset_config)
     ans = 0
     def collate_fn(samples):
@@ -338,7 +336,7 @@ def test_InternVLParquetDataset(sources):
         dataset=dataset,
         batch_size=1,
         shuffle=False,
-        num_workers=100,
+        num_workers=1,
         collate_fn=collate_fn
     )
     for iteration, batch in enumerate(dataloader):
@@ -348,6 +346,7 @@ def test_InternVLParquetDataset(sources):
             except:
                 print(k, v)
             print("=" * 10)
+        if iteration == 20: break
         
         
 '''
@@ -372,18 +371,5 @@ def test_InternVLParquetDataset(sources):
 '''
 
 if __name__ == "__main__":
-    data_file = sys.argv[1]
-    output_file = sys.argv[2] if len(sys.argv) > 2 else "results.txt"
-    hdfs_dirs = []
-    with open(data_file) as fp:
-        for line in fp:
-            if line.strip() != "":
-                hdfs_dirs.append(line.strip())
-    test_files = []
-    for fn in hdfs_dirs:
-        fn_list = shell_hdfs_ls(fn)
-        all_files = [fn for fn in fn_list if fn.endswith(".parquet")]
-        test_files.extend(all_files[:1])
-    print(test_files)
-    test_InternVLParquetDataset(test_files)
+    test_InternVLParquetDataset()
 
