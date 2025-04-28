28324bab8f0fee2531b29b5d112948a03cc8f86d
lingzhixin <lingzhixin@kuaishou.com>
2025-04-29 03:32:02 +0800

    email=lingzhixin@kuaishou.com,time=20250429 03:32:02,script=examples/vlm/qwen_vs_intern/debug_intern_2B_1u_sp1_16k_xray_nocut_moreu3.sh,node=1,comment=run internvl 2b stage1 by ccl,output=/llm_reco/lingzhixin/output/internvl-2b/qwen_vs_intern/debug_intern_2B_1u_sp1_16k_xray_nocut_moreu3_exp2

--- recovlm/data/datasets.py
+++ recovlm/data/datasets.py
@@ -2852,8 +2852,8 @@ class InternVLChatCompletionVisionDataset(IterableDataset):
       "sample_idx": packed_sample_idx.to(torch.int32)
     }
 
-    print(88988, input_ids.flatten().shape[0], pixel_values.shape[0] * 256, pixel_values.shape)
-    if input_ids.flatten().shape[0] < pixel_values.shape[0] * 256:
+    print(88988, packed_input_ids.flatten().shape[0], packed_pixel_values.shape[0] * 256, packed_pixel_values.shape)
+    if packed_input_ids.flatten().shape[0] < packed_pixel_values.shape[0] * 256:
       print_input_info(
         inputs,
         "inputs111: "
