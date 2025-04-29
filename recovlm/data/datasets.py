f999b42257e3e50be7c924bdfdedbda837162d5d
lingzhixin <lingzhixin@kuaishou.com>
2025-04-29 20:32:56 +0800

    email=lingzhixin@kuaishou.com,time=20250429 20:32:56,script=examples/vlm/qwen_profile/debug_qwen25_7B_zero2_cutd.sh,node=1,comment=version:0.4.1;model_size:72B;GPU_type:H800;data:inner & outer comments,output=/llm_reco/lingzhixin/exp_outputs/qwen_profile/debug/0.0.1/debug_qwen25_7B_zero2_cutd, resume

--- recovlm/data/datasets.py
+++ recovlm/data/datasets.py
@@ -1268,7 +1268,9 @@ _append_sample_packing_inputs:   Tensor: shape=(3, 1, 92), dtype=torch.int64, de
       if len(vision_starts) and len(vision_starts) > len(vision_ends): # 说明图片不完整
         inputs["input_ids"][:, vision_starts[-1]:] = 0
         inputs["loss_mask"][:, vision_starts[-1]:] = 0
-        n_tokens = inputs["image_grid_thw"][len(vision_ends)]
+        n_tokens_hw = inputs["image_grid_thw"][len(vision_ends)]
+        print(99898432423, n_tokens_hw)
+        n_tokens = n_tokens_hw[1] * n_tokens_hw[2]
         inputs["image_grid_thw"] = inputs["image_grid_thw"][:len(vision_ends)]
         inputs["pixel_values"] = inputs["pixel_values"][-n_tokens:]
       
