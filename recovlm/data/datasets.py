c6eb4919b719cfcc6d5f2c3e75f3c85d17b3ec25
lingzhixin <lingzhixin@kuaishou.com>
2025-04-29 22:18:12 +0800

    email=lingzhixin@kuaishou.com,time=20250429 22:18:12,script=examples/vlm/qwen_vs_intern/debug_intern_2B_1u_sp1_16k_xray_cut_moreu4_monitor_image_tokens_d.sh,node=1,comment=run internvl 2b stage1 by ccl,output=/llm_reco/huangsui/output/internvl-2b/qwen_vs_intern/debug_intern_2B_1u_sp1_16k_xray_cut_moreu4_monitor_image_tokens_d

--- recovlm/data/datasets.py
+++ recovlm/data/datasets.py
@@ -3061,7 +3061,7 @@ class InternVLChatCompletionVisionDataset(IterableDataset):
         t1 = time.perf_counter()
         candidates = self._find_in_range(input_ids_len, self.max_length, delta, target_count)
         input_ids_len = [sum(buffer[idx]["input_ids"].shape[-1] for idx in candidate) for candidate in candidates]
-        image_len = [sum(buffer[idx]["pixel_values"].size(0) * 256 for idx in candidate) for candidate in candidates]
+        image_len = [sum(buffer[idx]["pixel_values"].size(0) for idx in candidate) for candidate in candidates]
         #if dist.get_rank() == 0:
         print(f"[rank={dist.get_rank()}]  selected_candidates: {candidates}, input_ids: {input_ids_len}, images: {image_len}")
         t2 = time.perf_counter()
