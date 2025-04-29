7bf36db2bf85c022aa6afb90a180cfbf13556fbc
lingzhixin <lingzhixin@kuaishou.com>
2025-04-29 19:23:22 +0800

    email=lingzhixin@kuaishou.com,time=20250429 19:23:22,script=examples/vlm/qwen_profile/debug_qwen25_7B_zero2_cutd.sh,node=1,comment=version:0.4.1;model_size:72B;GPU_type:H800;data:inner & outer comments,output=/llm_reco/lingzhixin/exp_outputs/qwen_profile/debug/0.0.1/debug_qwen25_7B_zero2_cutd, resume

--- recovlm/data/datasets.py
+++ recovlm/data/datasets.py
@@ -1187,7 +1187,8 @@ class ChatCompletionVisionDataset(IterableDataset):
         }
         , prefix="_append_sample_packing_packed: ")
     
-    if self.cut_to_pad:
+    packable_length = self.max_length - cu_seqlens[-1]
+    if self.cut_to_pad and inputs['input_ids'].shape[1] > packable_length:
       """
 Qwen2TokenizerFast(name_or_path='/llm_reco_ssd/zhouyang12/models/Qwen2.5-VL-7B-Instruct/', vocab_size=151643, model_max_length=131072, is_fast=True, padding_side='right', truncation_side='right', special_tokens={'eos_token': '<|im_end|>', 'pad_token': '<|endoftext|>', 'additional_special_tokens': ['<|im_start|>', '<|im_end|>', '<|object_ref_start|>', '<|object_ref_end|>', '<|box_start|>', '<|box_end|>', '<|quad_start|>', '<|quad_end|>', '<|vision_start|>', '<|vision_end|>', '<|vision_pad|>', '<|image_pad|>', '<|video_pad|>']}, clean_up_tokenization_spaces=False, added_tokens_decoder={
         151643: AddedToken("<|endoftext|>", rstrip=False, lstrip=False, single_word=False, normalized=False, special=True),
@@ -1242,9 +1243,11 @@ _append_sample_packing_inputs:   Tensor: shape=(1, 92), dtype=torch.int64, devic
 _append_sample_packing_inputs: 'position_ids':
 _append_sample_packing_inputs:   Tensor: shape=(3, 1, 92), dtype=torch.int64, device=cpu, data=tensor([0, 1, 2, 3])...tensor([70, 71, 72, 73])
       """
-      packable_length = self.max_length - cu_seqlens[-1]
+      
       inputs["input_ids"] = inputs["input_ids"][:, :packable_length]
       inputs["loss_mask"] = inputs["loss_mask"][:, :packable_length]
+
+      next_position_id = inputs["position_ids"][packable_length]
       inputs["position_ids"] = inputs["position_ids"][..., :packable_length]
 
       vision_starts = torch.nonzero(inputs["input_ids"][0] == self.vision_start_token_id)
@@ -1252,7 +1255,10 @@ _append_sample_packing_inputs:   Tensor: shape=(3, 1, 92), dtype=torch.int64, de
       if len(vision_starts) and len(vision_starts) > len(vision_ends): # 说明图片不完整
         inputs["input_ids"][:, vision_starts[-1]:] = 0
         inputs["loss_mask"][:, vision_starts[-1]:] = 0
-        inputs["image_grid_thw"] = inputs["image_grid_thw"][:len(vision_ends)]
+        inputs["image_grid_thw"] = inputs["image_grid_thw"][..., :len(vision_ends)]
+        inputs["position_ids"] = inputs["position_ids"][..., :len(vision_ends)]
+
+
 
 
 
@@ -1641,7 +1647,7 @@ class ChatCompletionVisionDpoDataset(IterableDataset):
         images=image_inputs,
         videos=video_inputs,
         return_tensors="pt"
-
+    )
 
     # For the Warning: (add by zzx)
     #   Token indices sequence length is longer than the specified maximum 
@@ -1744,11 +1750,11 @@ class ChatCompletionVisionDpoDataset(IterableDataset):
         return_tensors="pt"
     )
     )
-    if dist.get_rank() == 0:
+    if dist.get_rank() == 0 and len(text) < 50:
       print('=' * 100)
       print(1111)
-      print_input_info(text, "134_text:", max_show=999)
-      print_input_info(inputs, "134_inputs:", max_show=999)
+      print_input_info(text, "134_text:", max_show=9999)
+      print_input_info(inputs, "134_inputs:", max_show=9999)
       print('=' * 100)
     # For the Warning: (add by zzx)
     #   Token indices sequence length is longer than the specified maximum 
