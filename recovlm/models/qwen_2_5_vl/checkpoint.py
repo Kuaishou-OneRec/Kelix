fa9623c91863c0391ae4425c8d05559b6da42432
maosiyang <you@kuaishou.com>
2025-04-30 11:44:24 +0800

    na

--- recovlm/models/qwen_2_5_vl/checkpoint.py
+++ recovlm/models/qwen_2_5_vl/checkpoint.py
@@ -143,8 +143,8 @@ class Qwen2_5_VL_moonvitCheckpointConverter(CheckpointConverter):
 
   def __call__(self,
                state_dict: Dict[str, torch.Tensor]) -> Dict[str, torch.Tensor]:
-    num_heads = self.config.num_heads
-    hidden_size = self.config.embed_dim
+    num_heads = self.config.num_attention_heads
+    hidden_size = self.config.hidden_size
     print(f"Converting from {self.model_path_or_name}")
     for k, v in tqdm.tqdm(state_dict.items()):
       if re.match(r"visual\.encoder\.blocks\.\d+\.wqkv\.weight", k):
