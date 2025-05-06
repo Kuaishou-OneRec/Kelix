ae55edf399858fc7954b42db8a1d3744d011f9a8
maosiyang <you@kuaishou.com>
2025-05-06 11:59:55 +0800

    na

--- recipes/train_fsdp.py
+++ recipes/train_fsdp.py
@@ -519,6 +519,8 @@ def train():
 
 
   with set_default_dtype(torch.bfloat16), torch.device("meta"):
+    print('msy1')
+    print(eval(args.model_class))
     model = eval(args.model_class).from_pretrained(
       args.model_dir, _attn_implementation="flash_attention_2",use_cache = False, ignore_mismatched_sizes=True
     )
