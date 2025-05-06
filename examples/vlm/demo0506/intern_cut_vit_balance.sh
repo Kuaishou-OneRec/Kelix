43fdb29ca3f4b793e73851b8b817699244d1f534
root <you@kuaishou.com>
2025-05-07 01:36:26 +0800

    email=you@kuaishou.com,time=20250507 01:36:26,script=examples/vlm/demo0506/intern_cut_vit_balance.sh,node=10,comment=run internvl 2b stage1 by ccl,output=/llm_reco/lingzhixin/exp_outputs/demo0506/intern_cut_vit_balance/0.0.1/

--- examples/vlm/demo0506/intern_cut_vit_balance.sh
+++ examples/vlm/demo0506/intern_cut_vit_balance.sh
@@ -121,7 +121,7 @@ nohup mpirun --allow-run-as-root \
                 --monitor_datasource_loss \
                 --monitor_datasource_cnt \
                 --dataset_config examples/vlm/demo0506/intern_cut_vit_balance.json \
-		--max_length 21000 \
+		--max_length 23000 \
                 --learning_rate 2e-5 \
                 --model_class InternVLChatModel \
                 --min_lr 0.0 \
