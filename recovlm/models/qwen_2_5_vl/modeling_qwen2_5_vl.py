276133bc0a0170df9129ba303b3010146c012046
maosiyang <you@kuaishou.com>
2025-05-06 18:08:34 +0800

    debug

--- recovlm/models/qwen_2_5_vl/modeling_qwen2_5_vl.py
+++ recovlm/models/qwen_2_5_vl/modeling_qwen2_5_vl.py
@@ -2522,16 +2522,15 @@ class Qwen2_5_VLForConditionalGeneration_moonvit(Qwen2_5_VLPreTrainedModel, Gene
                 image_grid_hws = torch.tensor(image_grid_hws,dtype=torch.int32,device=pixel_values.device)
                 image_embeds = self.visual(pixel_values, image_grid_hws)
                 image_embeds = self.mlp_AR(image_embeds)
-                print('msy_image_embeds',image_embeds.shape)
-                print('msy_image_embeds',image_embeds)
+
                 #64*7168
                 
                 n_image_tokens = (input_ids == self.config.image_token_id).sum().item()
                 #image_embeds is a list of tensor, each tensor is a image feature,I want to concat them all into a tensor
                 image_embeds = torch.cat(image_embeds,dim=0)
+                print('msy_image_embeds',image_embeds.shape)
+                print('msy_image_embeds',image_embeds)
                 n_image_features = image_embeds.shape[0]
-                print(f"n_image_features: {n_image_features}")
-                print(f"n_image_tokens: {n_image_tokens}")
                 if n_image_tokens != n_image_features:
                     raise ValueError(
                         f"Image features and image tokens do not match: tokens: {n_image_tokens}, features {n_image_features}"
