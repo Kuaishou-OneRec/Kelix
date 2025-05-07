dd201953d6fe3ed51e0f9593c281ca5ce7984002
maosiyang <you@kuaishou.com>
2025-05-07 15:19:54 +0800

    na

--- examples/vlm/debug_qwen25_7B_msy_test.sh
+++ examples/vlm/debug_qwen25_7B_msy_test.sh
@@ -110,7 +110,7 @@ nohup mpirun --allow-run-as-root -np $np \
                 --model_class Qwen2_5_VLForConditionalGeneration_moonvit \
                 --monitor_datasource_loss \
                 --monitor_datasource_cnt \
-                --max_length 15000 \
+                --max_length 20000 \
                 --learning_rate 1e-6 \
                 --min_lr 0.0 \
                 --weight_decay 0.1 \
