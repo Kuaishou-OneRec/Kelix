34ca6029e2401e8a7c3a1ef27070f4fd4ed2ae58
lingzhixin <lingzhixin@kuaishou.com>
2025-05-08 03:56:59 +0800

    email=lingzhixin@kuaishou.com,time=20250508 03:56:59,script=examples/vlm/throughput0507/intern_stage2.sh,node=23,comment=run internvl 2b stage1 by ccl,output=/llm_reco/lingzhixin/exp_outputs/throughput0507/intern_stage2/0.0.1/

--- examples/vlm/throughput0507/qwen_stage2_nocut.sh
+++ examples/vlm/throughput0507/qwen_stage2_nocut.sh
@@ -121,7 +121,7 @@ nohup mpirun --allow-run-as-root \
                 --model_class Qwen2_5_VLForConditionalGeneration \
                 --monitor_datasource_loss \
                 --monitor_datasource_cnt \
-                --max_length 17000 \
+                --max_length 16000 \
                 --learning_rate 1e-6 \
                 --min_lr 0.0 \
                 --weight_decay 0.1 \
