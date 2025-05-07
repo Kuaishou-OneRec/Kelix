1a0ffedbab2db9b847b38868a8abbd87af6ea3a6
root <you@kuaishou.com>
2025-05-08 01:47:46 +0800

    email=you@kuaishou.com,time=20250508 01:47:45,script=examples/vlm/throughput0507/intern_stage3_sm.sh,node=1,comment=run internvl 2b stage1 by ccl,output=/llm_reco/lingzhixin/exp_outputs/throughput0507/intern_stage3_sm/0.0.1/

--- recipes/train_fsdp.py
+++ recipes/train_fsdp.py
@@ -580,7 +580,8 @@ def train():
         args=(dataset_config, args.model_class, args.max_length, batch_queue, args))
     data_process.start()
     print(f"data process started")
-
+    import time; time.sleep(1000)
+    
   # init model params
   os.environ["KML_ID"] = args.kml_id
   os.environ["KML_TASK_ID"] = args.kml_task_id
