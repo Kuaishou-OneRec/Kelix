d713531249ba38aae2af1385b4c1a1f67314815e
root <you@kuaishou.com>
2025-05-07 15:48:16 +0800

    email=you@kuaishou.com,time=20250507 15:48:15,script=examples/vlm/0.7.1/internvl_2b_stage_3_v3.sh,node=7,comment=run internvl 2b 0.7.0 stage2 by lzx, load from ckpt1k, use stage2-0.6.0 data, note there stage1.5 0.6.0 data is part of stage2 0.6.0 data,output=/llm_reco_ssd/luoxinchen/output3/RecoVLM-Base/0.7.1/2b/stage_3_v3

--- recovlm/data/dataloaders.py
+++ recovlm/data/dataloaders.py
@@ -269,6 +269,7 @@ def get_chat_completion_vision_parquet_dataloader(sources: str,
         max_retry=max_retry,
         multiple_of=multiple_of,
         datasource_config=datasource_config,
+        vit_token_balance=vit_token_balance,
         **kwargs
         )
 
