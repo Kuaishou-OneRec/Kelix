088100a4e32c66bb7dfd59b7f2b4392da7149605
maosiyang <you@kuaishou.com>
2025-05-08 15:39:24 +0800

    pull 0.7.0_2

--- recovlm/data/dataloaders.py
+++ recovlm/data/dataloaders.py
@@ -274,7 +274,6 @@ def get_chat_completion_vision_parquet_dataloader(sources: str,
         shrink_ratio=shrink_ratio,
         max_retry=max_retry,
         multiple_of=multiple_of,
-        cut_to_pad=cut_to_pad,
         datasource_config=datasource_config,
         vit_token_balance=vit_token_balance,
         **kwargs
