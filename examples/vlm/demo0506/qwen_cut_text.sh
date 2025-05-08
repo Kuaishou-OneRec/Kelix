ca9a8b7d37a28e5f3e4c5e8c2cc8c52cb9530f7f
maosiyang <you@kuaishou.com>
2025-05-08 20:42:29 +0800

    pull 0.7.0_2

--- examples/vlm/demo0506/qwen_cut_text.sh
+++ examples/vlm/demo0506/qwen_cut_text.sh
@@ -16,7 +16,7 @@ sed 's/=1/=8/g' /etc/mpi/hostfile  | head -1000 > /etc/mpi/hostfile_seq
 
 # MODEL_DIR=/llm_reco_ssd/luoxinchen/output/RecoVLM/Qwen2-VL-7B-stage1-v0.0.36/global_step90000-hf
 MODEL_DIR=/llm_reco_ssd/zhouyang12/models/Qwen2.5-VL-7B-Instruct # Pretrained/Base model path
-OUTPUT_DIR=/llm_reco/lingzhixin/exp_outputs/demo0506/qwen_cut_text/0.0.1/
+OUTPUT_DIR=/llm_reco/maosiyang/output/freeze_debug/0.0.3/baseline
 
 mkdir -p $OUTPUT_DIR
 
@@ -121,7 +121,7 @@ nohup mpirun --allow-run-as-root \
                 --model_class Qwen2_5_VLForConditionalGeneration \
                 --monitor_datasource_loss \
                 --monitor_datasource_cnt \
-                --max_length 64000 \
+                --max_length 17000 \
                 --learning_rate 1e-6 \
                 --min_lr 0.0 \
                 --weight_decay 0.1 \
@@ -129,7 +129,7 @@ nohup mpirun --allow-run-as-root \
                 --num_warmup_steps 500 \
                 --num_training_steps 50000 \
                 --save_checkpoint_per_step 500 \
-                --sequence_parallel_size 4 \
+                --sequence_parallel_size 1 \
                 --use_flash_attention_2 \
                 --logging_per_step 10 \
                 --fp32_weight \
