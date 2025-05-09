ff9d08cea852d9259a624435a06ed9f162ed5b30
maosiyang <you@kuaishou.com>
2025-05-09 14:54:47 +0800

    na

--- recipes/qwen_moonvit_checkpoint_save.py
+++ recipes/qwen_moonvit_checkpoint_save.py
@@ -1,5 +1,5 @@
-from recovlm.models.qwen_2_5_vl.modeling_qwen2_5_vl import Qwen2_5_VLForConditionalGeneration_moonvit,Qwen2_5_VLForConditionalGeneration
-from recovlm.models.qwen_2_5_vl.processing_qwen2_5_vl import Qwen2_5_VLProcessor_moonvit 
+from recovlm.models.qwen_2_5_vl.modeling_qwen2_5_vl import Qwen2_5_VLForConditionalGeneration_moonvit,Qwen2_5_VLForConditionalGeneration_siglip
+from recovlm.models.qwen_2_5_vl.processing_qwen2_5_vl import Qwen2_5_VLProcessor_moonvit,Qwen2_5_VLProcessor_siglip
 import torch
 from PIL import Image
 from recipes.ViT.training.models.MoonVision.image_processing_kimi_vl import KimiVLImageProcessor_for_qwen2_5_vl
@@ -19,17 +19,17 @@ import json
 
 
 def save_model_state(dict_state):
-    torch.save(dict_state, "/llm_reco/maosiyang/model/qwen_moonvit/qwen2_5_vl_moonvit_state_dict.pth")
+    torch.save(dict_state, "/llm_reco/maosiyang/model/qwen_moonvit/qwen2_5_vl_siglip_state_dict.pth")
 
 # 加载模型状态的示例代码
 def load_model_state():
     # 1. 首先初始化一个模型实例
-    loaded_model = Qwen2_5_VLForConditionalGeneration_moonvit.from_pretrained(
-        "/llm_reco_ssd/zhouyang12/models/Qwen2-VL-7B-Instruct",
+    loaded_model = Qwen2_5_VLForConditionalGeneration_siglip.from_pretrained(
+        "/llm_reco_ssd/zhouyang12/models/Qwen2.5-VL-7B-Instruct",
         ignore_mismatched_sizes=True
     )
     # 2. 加载保存的state dict
-    state_dict = torch.load("/llm_reco/maosiyang/model/qwen_moonvit/qwen2_5_vl_moonvit_state_dict.pth")
+    state_dict = torch.load("/llm_reco/maosiyang/model/qwen_moonvit/qwen2_5_vl_siglip_state_dict.pth")
     
     # 3. 将state dict加载到模型中
     print('=================================')
@@ -43,11 +43,21 @@ def load_model_state():
 
 if __name__ == "__main__":
 
-    model = Qwen2_5_VLForConditionalGeneration_moonvit.from_pretrained(
-      "/llm_reco_ssd/zhouyang12/models/Qwen2-VL-7B-Instruct",ignore_mismatched_sizes=True
+    model = Qwen2_5_VLForConditionalGeneration_siglip.from_pretrained(
+      "/llm_reco_ssd/zhouyang12/models/Qwen2.5-VL-7B-Instruct",ignore_mismatched_sizes=True
     )
-    pt = '/llm_reco/liuyang76/Models/MoonVitParam/MoonVit.pt'
-    visual_state_dict = torch.load(pt)
+    from safetensors import safe_open
+
+    with safe_open("/llm_reco/liuyang76/Models/siglip2-so400m-patch14-384/model.safetensors", framework="pt", device="cpu") as f:
+        pt = {}
+        for key in f.keys():
+            if "packing" in key:
+                continue
+            pt[key] = f.get_tensor(key)
+    # ckpt = '/llm_reco/liuyang76/Models/siglip2-so400m-patch14-384/model.safetensors'
+    # #convert to pt
+    # pt = '/llm_reco/liuyang76/Models/MoonVitParam/MoonVit.pt'
+    visual_state_dict = pt
     # for key, value in visual_state_dict.items():
     #     print('--------------------------------')
     #     print(key, value.shape)
