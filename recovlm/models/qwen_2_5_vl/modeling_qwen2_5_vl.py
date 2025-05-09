4294832abc0aca91f61d2e1f03976c65c48e5fe9
maosiyang <you@kuaishou.com>
2025-05-09 15:09:03 +0800

    na

--- recovlm/models/qwen_2_5_vl/modeling_qwen2_5_vl.py
+++ recovlm/models/qwen_2_5_vl/modeling_qwen2_5_vl.py
@@ -2809,7 +2809,8 @@ class Qwen2_5_VLForConditionalGeneration_siglip(Qwen2_5_VLPreTrainedModel, Gener
     def __init__(self, config):
         super().__init__(config)
         Siglip_config = SiglipConfig.from_pretrained('/llm_reco/liuyang76/Models/siglip2-so400m-patch14-384')
-        Siglip_config = SiglipConfig.vision_config
+        # print('msy_siglip_config',Siglip_config)
+        Siglip_config = Siglipconfig.vision_config
         #Siglip_config._attn_implementation = 'flash_attention_2'
         self.mlp_AR = Projector(config,Siglip_config)
         self.visual = SiglipPretrainedModel(Siglip_config)
