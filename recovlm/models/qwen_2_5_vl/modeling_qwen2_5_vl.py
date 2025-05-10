e50bdbf7f5722246fc240f42b5e83b7a97c1fa4d
zangdunju <zangdunju@kuaishou.com>
2025-05-10 23:50:17 +0800

    email=zangdunju@kuaishou.com,time=20250510 23:50:16,script=examples/vlm/debug_qwen25_7B_msy_test.sh,node=1,comment=version:0.4.1;model_size:72B;GPU_type:H800;data:inner & outer comments,output=/llm_reco/zangdunju/output2/freeze_debug/0.0.3/qwen25_7B_qwen_processor, resume

--- recovlm/models/qwen_2_5_vl/modeling_qwen2_5_vl.py
+++ recovlm/models/qwen_2_5_vl/modeling_qwen2_5_vl.py
@@ -3098,8 +3098,8 @@ class Qwen2_5_VLForConditionalGeneration_siglip(Qwen2_5_VLPreTrainedModel, Gener
                 #image_grid_hws = image_grid_thw.prod(dim=1)#elimate the temporal dimension
                 
                 for thw in image_grid_thw:
-                    print(thw)
-                    image_grid_hws.append((thw[0], thw[1],thw[2]))
+                    thw_tuple = tuple(thw.detach().cpu().numpy().tolist())
+                    image_grid_hws.append(thw_tuple)
                     image_position_ids = torch.arange(np.prod(thw)) % np.prod(thw[1:])
                     siglip_position_ids.append(image_position_ids)
                 siglip_position_ids = torch.concat(siglip_position_ids, dim=0).to(pixel_values.device)
