40a7d6cff4ccaaa0b3790b841492e2c82271e3ae
zangdunju <zangdunju@kuaishou.com>
2025-05-10 11:51:23 +0000

    email=zangdunju@kuaishou.com,time=20250510 11:51:23,script=examples/vlm/debug_qwen25_7B_msy_test.sh,node=1,comment=version:0.4.1;model_size:72B;GPU_type:H800;data:inner & outer comments,output=/llm_reco/zangdunju/output2/freeze_debug/0.0.3/qwen25_7B_qwen_processor, resume

--- recipes/ViT/training/models/__init__.py
+++ recipes/ViT/training/models/__init__.py
@@ -1,4 +1,3 @@
-
 from recipes.ViT.training.models.kimi import KimiViT
 from recipes.ViT.training.models.kimi_siglip import KimiViT as KimiViTSigLIP
 from recipes.ViT.training.models.kimi_moonvit import KimiViT as KimiViTMoonViT
