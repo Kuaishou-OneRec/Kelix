de76cc05a25735a15159e947f0c3e16c5299cc2a
zangdunju <zangdunju@kuaishou.com>
2025-05-10 11:45:50 +0000

    email=zangdunju@kuaishou.com,time=20250510 11:45:50,script=examples/vlm/debug_qwen25_7B_msy_test.sh,node=1,comment=version:0.4.1;model_size:72B;GPU_type:H800;data:inner & outer comments,output=/llm_reco/zangdunju/output2/freeze_debug/0.0.3/qwen25_7B_qwen_processor, resume

--- recipes/ViT/training/models/__init__.py
+++ recipes/ViT/training/models/__init__.py
@@ -1,12 +1,6 @@
-<<<<<<< HEAD
-# from recipes.ViT.training.models.kimi import KimiViT
-# from recipes.ViT.training.models.kimi_siglip import KimiViT as KimiViTSigLIP
-# from recipes.ViT.training.models.VIVIT import KimiViViT
-# from recipes.ViT.training.models.moonvit.modeling_moonvit import MoonViTforSiglip
-=======
+
 from recipes.ViT.training.models.kimi import KimiViT
 from recipes.ViT.training.models.kimi_siglip import KimiViT as KimiViTSigLIP
 from recipes.ViT.training.models.kimi_moonvit import KimiViT as KimiViTMoonViT
 from recipes.ViT.training.models.VIVIT import KimiViViT
 from recipes.ViT.training.models.moonvit.modeling_moonvit import MoonViTforSiglip
->>>>>>> 3962ab8f6bed6ae146896938932dda0e389980c5
