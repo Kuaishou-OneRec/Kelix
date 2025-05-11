24bbd17afd01195555d0fb0426c263ba02298233
maosiyang <you@kuaishou.com>
2025-05-11 16:46:32 +0800

    add qwen3

--- recipes/test_load_model.py
+++ recipes/test_load_model.py
@@ -32,9 +32,9 @@
 
 
 
-from qwen2_5_vl.modeling_qwen2_5_vl import Qwen2_5_VLForConditionalGeneration
-
-model = Qwen2_5_VLForConditionalGeneration.from_pretrained('/llm_reco_ssd/zhouyang12/models/Qwen3-8B-Base')
+from recovlm.models.qwen_3_vl.modeling_qwen3_vl import Qwen3_VLForConditionalGeneration
+from recovlm.models.qwen_3_vl.processing_qwen2_5_vl import Qwen2_5_VLProcessor_siglip
+model = Qwen3_VLForConditionalGeneration.from_pretrained('/llm_reco_ssd/zhouyang12/models/msy_Qwen3vl-8B-Base')
 #tokenizer = AutoTokenizer.from_pretrained('/llm_reco_ssd/zhouyang12/models/Qwen3-8B-Base')
 
 model = AutoModelForCausalLM.from_pretrained(
@@ -43,24 +43,30 @@ model = AutoModelForCausalLM.from_pretrained(
     device_map="auto"
 )
 
-
-# prepare the model input
-prompt = "Give me a short introduction to large language model."
 messages = [
-    {"role": "user", "content": prompt}
+    {
+        "role": "user",
+        "content": [
+            {"type": "text", "text": "Give me a short introduction to large language model."},
+        ],
+    }
 ]
-text = tokenizer.apply_chat_template(
-    messages,
-    tokenize=False,
-    add_generation_prompt=True,
-    enable_thinking=True # Switches between thinking and non-thinking modes. Default is True.
+processor = Qwen2_5_VLProcessor_siglip()
+
+
+text = processor.apply_chat_template(
+    messages, tokenize=False, add_generation_prompt=True
 )
-model_inputs = tokenizer([text], return_tensors="pt").to(model.device)
+inputs = processor(
+    text=[text],
+    padding=True,
+    return_tensors="pt",
+)
 
 #print the logits
-logits2 = model(**model_inputs).logits
+logits2 = model(**inputs).logits
 
 #judge the logits are the same
-print(torch.allclose(logits1, logits2))
+# print(torch.allclose(logits1, logits2))
 
 
