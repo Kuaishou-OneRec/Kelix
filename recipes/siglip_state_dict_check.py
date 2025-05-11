f9a06a6eeaf8cfb293c8d35816c2b88bf482e2ef
maosiyang <you@kuaishou.com>
2025-05-11 20:22:12 +0800

    add qwen3

--- recipes/siglip_state_dict_check.py
+++ recipes/siglip_state_dict_check.py
@@ -3,10 +3,11 @@ from recovlm.models.qwen_3_vl.processing_qwen2_5_vl import Qwen2_5_VLProcessor_s
 model_name = "Qwen/Qwen3-8B"
 import json
 # load the tokenizer and the model
-MODEL_DIR="/llm_reco_ssd/zhouyang12/models/msy_Qwen3vl-8B-Base"
-processor = Qwen2_5_VLProcessor_siglip.from_pretrained(MODEL_DIR)
+processor = Qwen2_5_VLProcessor_siglip.from_pretrained(model_name)
 model = AutoModelForCausalLM.from_pretrained(
     model_name,
     torch_dtype="auto",
     device_map="auto"
 )
+for name, param in model.named_parameters():
+    print(name, param.shape)
