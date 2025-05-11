75339fbed1f65e1b49cb99e6d726c79201d389b1
maosiyang <you@kuaishou.com>
2025-05-11 19:22:26 +0800

    add qwen3

--- recipes/test_load_model_2.py
+++ recipes/test_load_model_2.py
@@ -1,9 +1,10 @@
 from transformers import AutoModelForCausalLM, AutoTokenizer
-
+from recovlm.models.qwen_3_vl.processing_qwen2_5_vl import Qwen2_5_VLProcessor_siglip
 model_name = "Qwen/Qwen3-8B"
 
 # load the tokenizer and the model
-tokenizer = AutoTokenizer.from_pretrained("/llm_reco_ssd/zhouyang12/models/Qwen2.5-VL-7B-Instruct")
+MODEL_DIR="/llm_reco_ssd/zhouyang12/models/msy_Qwen3vl-8B-Base"
+processor = Qwen2_5_VLProcessor_siglip.from_pretrained(MODEL_DIR)
 model = AutoModelForCausalLM.from_pretrained(
     model_name,
     torch_dtype="auto",
@@ -15,15 +16,20 @@ prompt = "Give me a short introduction to large language model."
 messages = [
     {"role": "user", "content": prompt}
 ]
+
+
+processor = Qwen2_5_VLProcessor_siglip.from_pretrained(MODEL_DIR)
+tokenizer = processor.tokenizer
 text = tokenizer.apply_chat_template(
     messages,
     tokenize=False,
     add_generation_prompt=True,
-    enable_thinking=False # Switches between thinking and non-thinking modes. Default is True.
+    enable_thinking=True # Switches between thinking and non-thinking modes. Default is True.
 )
 model_inputs = tokenizer([text], return_tensors="pt").to(model.device)
-print('inputs', model_inputs)
-# conduct text completion
+
+print('model_inputs', model_inputs)
+
 output = model(**model_inputs)
 
 logits = output.logits
