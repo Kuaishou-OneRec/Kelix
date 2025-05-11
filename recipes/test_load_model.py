62826ce3c434462146b221ce78e2c4941e7b977b
maosiyang <you@kuaishou.com>
2025-05-11 15:37:35 +0800

    add qwen3

--- recipes/test_load_model.py
+++ recipes/test_load_model.py
@@ -1,38 +1,38 @@
-from transformers import AutoModelForCausalLM, AutoTokenizer
+# from transformers import AutoModelForCausalLM, AutoTokenizer
 
-model_name = "Qwen/Qwen3-8B"
+# model_name = "Qwen/Qwen3-8B"
 
-# load the tokenizer and the model
-tokenizer = AutoTokenizer.from_pretrained(model_name)
-model = AutoModelForCausalLM.from_pretrained(
-    model_name,
-    torch_dtype="auto",
-    device_map="auto"
-)
+# # load the tokenizer and the model
+# tokenizer = AutoTokenizer.from_pretrained(model_name)
+# model = AutoModelForCausalLM.from_pretrained(
+#     model_name,
+#     torch_dtype="auto",
+#     device_map="auto"
+# )
 
-# prepare the model input
-prompt = "Give me a short introduction to large language model."
-messages = [
-    {"role": "user", "content": prompt}
-]
-text = tokenizer.apply_chat_template(
-    messages,
-    tokenize=False,
-    add_generation_prompt=True,
-    enable_thinking=True # Switches between thinking and non-thinking modes. Default is True.
-)
-model_inputs = tokenizer([text], return_tensors="pt").to(model.device)
+# # prepare the model input
+# prompt = "Give me a short introduction to large language model."
+# messages = [
+#     {"role": "user", "content": prompt}
+# ]
+# text = tokenizer.apply_chat_template(
+#     messages,
+#     tokenize=False,
+#     add_generation_prompt=True,
+#     enable_thinking=True # Switches between thinking and non-thinking modes. Default is True.
+# )
+# model_inputs = tokenizer([text], return_tensors="pt").to(model.device)
 
-#print the logits
-logits1 = model(**model_inputs).logits
+# #print the logits
+# logits1 = model(**model_inputs).logits
 
-#torch.Size([1, 18, 151936])
-
-
+# #torch.Size([1, 18, 151936])
 
 
 
-from recovlm.models.qwen3.modeling_qwen3 import Qwen3ForCausalLM
+
+
+from recovlm.models.qwen3_siglip.modeling_qwen3_siglip import Qwen3ForCausalLM
 
 model = Qwen3ForCausalLM.from_pretrained('/llm_reco_ssd/zhouyang12/models/Qwen3-8B-Base')
 tokenizer = AutoTokenizer.from_pretrained('/llm_reco_ssd/zhouyang12/models/Qwen3-8B-Base')
