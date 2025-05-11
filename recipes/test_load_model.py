2d3d0aae700583b7606467e558bd3b5c619ee112
maosiyang <you@kuaishou.com>
2025-05-11 18:29:20 +0800

    add qwen3

--- recipes/test_load_model.py
+++ recipes/test_load_model.py
@@ -276,7 +276,10 @@ def debug_model_inference(model):
 
     # add generation config
 
-    output = model(**inputs); exit()
+    output = model(**inputs); 
+    print_rank_0(output)
+    
+    exit()
     generation_config = GenerationConfig(
     max_new_tokens=128,
     do_sample=True,
