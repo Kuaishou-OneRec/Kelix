0a03a9e3ffaee1ab045a937883b0889b58c57a55
maosiyang <you@kuaishou.com>
2025-05-11 20:16:18 +0800

    add qwen3

--- recipes/siglip_state_dict_check.py
+++ recipes/siglip_state_dict_check.py
@@ -1,31 +1,12 @@
-from recovlm.models.qwen_2_5_vl.modeling_qwen2_5_vl import Qwen2_5_VLForConditionalGeneration_moonvit,Qwen2_5_VLForConditionalGeneration
-from recovlm.models.qwen_2_5_vl.processing_qwen2_5_vl import Qwen2_5_VLProcessor_moonvit 
-import torch
-from PIL import Image
-from recipes.ViT.training.models.MoonVision.image_processing_kimi_vl import KimiVLImageProcessor_for_qwen2_5_vl
-from qwen_vl_utils import process_vision_info
-from recipes.ViT.training.models.MoonVision.modeling_kimi_vl import MoonVitPretrainedModel
-# # from recovlm.models.qwen_2_5_vl.modeling_qwen2_5_vl import Qwen2_5_VLForConditionalGeneration_moonvit
+from transformers import AutoModelForCausalLM, AutoTokenizer
+from recovlm.models.qwen_3_vl.processing_qwen2_5_vl import Qwen2_5_VLProcessor_siglip
+model_name = "Qwen/Qwen3-8B"
 import json
-
-from recipes.ViT.training.models.siglip.modeling_siglip import SiglipVisionModel
-
-# #config = json.load(open("/llm_reco_ssd/zhouyang12/models/Qwen2.5-7B-Instruct/config.json", "r"))
-# #model = Qwen2_5_VLForConditionalGeneration_moonvit(config)
-# model = \
-# Qwen2_5_VLForConditionalGeneration_moonvit.from_pretrained("/llm_reco_ssd/zhouyang12/models/Qwen2.5-7B-Instruct/",
-#          ignore_mismatched_sizes=True)
-# for key, value in model.named_parameters():
-#     print(key, value.shape)
-
-
-
-if __name__ == "__main__":
-
-    model = SiglipVisionModel.from_pretrained(
-      "/llm_reco/liuyang76/Models/siglip2-so400m-patch14-384",ignore_mismatched_sizes=True
-    )
-    for key, value in model.named_parameters():
-        print(key, value.shape)
-
-
+# load the tokenizer and the model
+MODEL_DIR="/llm_reco_ssd/zhouyang12/models/msy_Qwen3vl-8B-Base"
+processor = Qwen2_5_VLProcessor_siglip.from_pretrained(MODEL_DIR)
+model = AutoModelForCausalLM.from_pretrained(
+    model_name,
+    torch_dtype="auto",
+    device_map="auto"
+)
