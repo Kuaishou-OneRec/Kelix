6ac86b78a8f55b4153eca50cfe91972b43e8ef27
maosiyang <you@kuaishou.com>
2025-05-11 15:00:13 +0800

    add qwen3

--- recipes/test_load_model.py
+++ recipes/test_load_model.py
@@ -26,3 +26,41 @@ model_inputs = tokenizer([text], return_tensors="pt").to(model.device)
 #print the logits
 logits = model(**model_inputs).logits
 print(logits.shape)
+
+
+
+
+
+
+
+# from recovlm.models.qwen3.modeling_qwen3 import Qwen3ForCausalLM
+
+# model = Qwen3ForCausalLM.from_pretrained('/llm_reco_ssd/zhouyang12/models/Qwen3-8B-Base')
+# tokenizer = AutoTokenizer.from_pretrained('/llm_reco_ssd/zhouyang12/models/Qwen3-8B-Base')
+
+# model = AutoModelForCausalLM.from_pretrained(
+#     model_name,
+#     torch_dtype="auto",
+#     device_map="auto"
+# )
+
+
+
+
+# # prepare the model input
+# prompt = "Give me a short introduction to large language model."
+# messages = [
+#     {"role": "user", "content": prompt}
+# ]
+# text = tokenizer.apply_chat_template(
+#     messages,
+#     tokenize=False,
+#     add_generation_prompt=True,
+#     enable_thinking=True # Switches between thinking and non-thinking modes. Default is True.
+# )
+# model_inputs = tokenizer([text], return_tensors="pt").to(model.device)
+
+# #print the logits
+# logits = model(**model_inputs).logits
+# print(logits.shape)
+
