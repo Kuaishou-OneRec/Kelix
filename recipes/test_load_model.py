2ac821e4a0f8ac76dbfcfda1475f899cc8d24a42
maosiyang <you@kuaishou.com>
2025-05-11 15:16:59 +0800

    add qwen3

--- recipes/test_load_model.py
+++ recipes/test_load_model.py
@@ -24,43 +24,43 @@ text = tokenizer.apply_chat_template(
 model_inputs = tokenizer([text], return_tensors="pt").to(model.device)
 
 #print the logits
-logits = model(**model_inputs).logits
-print(logits.shape)
+logits1 = model(**model_inputs).logits
+
+#torch.Size([1, 18, 151936])
 
 
 
 
 
+from recovlm.models.qwen3.modeling_qwen3 import Qwen3ForCausalLM
+
+model = Qwen3ForCausalLM.from_pretrained('/llm_reco_ssd/zhouyang12/models/Qwen3-8B-Base')
+tokenizer = AutoTokenizer.from_pretrained('/llm_reco_ssd/zhouyang12/models/Qwen3-8B-Base')
+
+model = AutoModelForCausalLM.from_pretrained(
+    model_name,
+    torch_dtype="auto",
+    device_map="auto"
+)
 
 
-# from recovlm.models.qwen3.modeling_qwen3 import Qwen3ForCausalLM
+# prepare the model input
+prompt = "Give me a short introduction to large language model."
+messages = [
+    {"role": "user", "content": prompt}
+]
+text = tokenizer.apply_chat_template(
+    messages,
+    tokenize=False,
+    add_generation_prompt=True,
+    enable_thinking=True # Switches between thinking and non-thinking modes. Default is True.
+)
+model_inputs = tokenizer([text], return_tensors="pt").to(model.device)
 
-# model = Qwen3ForCausalLM.from_pretrained('/llm_reco_ssd/zhouyang12/models/Qwen3-8B-Base')
-# tokenizer = AutoTokenizer.from_pretrained('/llm_reco_ssd/zhouyang12/models/Qwen3-8B-Base')
+#print the logits
+logits2 = model(**model_inputs).logits
 
-# model = AutoModelForCausalLM.from_pretrained(
-#     model_name,
-#     torch_dtype="auto",
-#     device_map="auto"
-# )
+#judge the logits are the same
+print(torch.allclose(logits1, logits2))
 
-
-
-
-# # prepare the model input
-# prompt = "Give me a short introduction to large language model."
-# messages = [
-#     {"role": "user", "content": prompt}
-# ]
-# text = tokenizer.apply_chat_template(
-#     messages,
-#     tokenize=False,
-#     add_generation_prompt=True,
-#     enable_thinking=True # Switches between thinking and non-thinking modes. Default is True.
-# )
-# model_inputs = tokenizer([text], return_tensors="pt").to(model.device)
-
-# #print the logits
-# logits = model(**model_inputs).logits
-# print(logits.shape)
 
