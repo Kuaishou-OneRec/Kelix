db2b9fe1c0110a7af82159d0503fdde7c33077ef
maosiyang <you@kuaishou.com>
2025-05-11 20:23:00 +0800

    add qwen3

--- recipes/siglip_state_dict_check.py
+++ recipes/siglip_state_dict_check.py
@@ -2,8 +2,7 @@ from transformers import AutoModelForCausalLM, AutoTokenizer
 from recovlm.models.qwen_3_vl.processing_qwen2_5_vl import Qwen2_5_VLProcessor_siglip
 model_name = "Qwen/Qwen3-8B"
 import json
-# load the tokenizer and the model
-processor = Qwen2_5_VLProcessor_siglip.from_pretrained(model_name)
+
 model = AutoModelForCausalLM.from_pretrained(
     model_name,
     torch_dtype="auto",
