bebf97026ad0ebf659195227e9283d973c3204af
maosiyang <you@kuaishou.com>
2025-05-12 16:21:05 +0800

    add qwen3

--- recipes/test_load_model.py
+++ recipes/test_load_model.py
@@ -274,7 +274,14 @@ def debug_model_inference(model):
     # Convert BFloat16 tensor to float32 before numpy conversion
     logits_np = logits.detach().cpu().float().numpy().tolist()
     json.dump(logits_np, open("logits1.json", "w"))
-
+    generated_ids = model.generate(**inputs, max_new_tokens=128)
+    generated_ids_trimmed = [
+        out_ids[len(in_ids) :] for in_ids, out_ids in zip(inputs.input_ids, generated_ids)
+    ]
+    output_text = processor.batch_decode(
+        generated_ids_trimmed, skip_special_tokens=True, clean_up_tokenization_spaces=False
+    )
+    print_rank_0(output_text)
     #print_rank_0(output)
     
     exit()
