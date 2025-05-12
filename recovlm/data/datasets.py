42fe1ec9c4ccb690587ea955924774dc948fc64e
zangdunju <zangdunju@kuaishou.com>
2025-05-12 18:24:28 +0800

    email=zangdunju@kuaishou.com,time=20250512 18:24:28,script=examples/vlm/qwen_vs_intern/debug_intern_2B_1u_sp1_16k_xray_cut_moreu4_monitor_image_tokens_d.sh,node=1,comment=run_internvl_2b_stage1_by_ccl,output=/llm_reco/huangsui/output/internvl-2b/qwen_vs_intern/hs184_intern_2B_1u_sp1_16k_xray_cut_moreu4_monitor_image_tokens_d

--- recovlm/data/datasets.py
+++ recovlm/data/datasets.py
@@ -3142,7 +3142,7 @@ class InternVLChatCompletionVisionDataset(IterableDataset):
         local_selected = selected[dist.get_rank()]
         found = -1
         for i in range(0, len(flops) // 2, 2):
-            if math.isclose(flops[2*i], local_selected[0]) and math.isclose(flops[2*i+1], local_selected[1]):
+            if math.isclose(flops[2*i], local_selected[0], rel_tol=1e-6) and math.isclose(flops[2*i+1], local_selected[1], rel_tol=1e-6):
                 found = i
                 break
         if found == -1:
