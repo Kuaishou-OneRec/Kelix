82c93608d0ef2836ea05406339a7dc02d79f7fa7
maosiyang <you@kuaishou.com>
2025-05-12 19:20:59 +0800

    add qwen3

--- recovlm/models/qwen_3_vl_2/modeling_qwen2_5_vl.py
+++ recovlm/models/qwen_3_vl_2/modeling_qwen2_5_vl.py
@@ -1219,18 +1219,18 @@ class Qwen2_5_VLDecoderLayer(nn.Module):
             **kwargs
         )
         hidden_states = residual + hidden_states
-        print("=============================")
+        print_rank_0("=============================")
         print_rank_0("qwen2_5_vl_decoder_layer_hidden_states", hidden_states)
-        print("=============================")
+        print_rank_0("=============================")
 
         # Fully Connected
         residual = hidden_states
         hidden_states = self.post_attention_layernorm(hidden_states)
         hidden_states = self.mlp(hidden_states)
         hidden_states = residual + hidden_states
-        print("=============================")
+        print_rank_0("=============================")
         print_rank_0("qwen2_5_vl_decoder_layer_hidden_states_2", hidden_states)
-        print("=============================")
+        print_rank_0("=============================")
 
         outputs = (hidden_states,)
 
@@ -1327,9 +1327,9 @@ class Qwen2_5_VLModel(Qwen2_5_VLPreTrainedModel):
         )
 
         hidden_states = inputs_embeds
-        print("=============================")
+        print_rank_0("=============================")
         print_rank_0("qwen2_5_vl_model_hidden_states", hidden_states)
-        print("=============================")
+        print_rank_0("=============================")
         # create position embeddings to be shared across the decoder layers
         position_embeddings = self.rotary_emb(hidden_states, position_ids)
 
@@ -1387,9 +1387,9 @@ class Qwen2_5_VLModel(Qwen2_5_VLPreTrainedModel):
                 all_self_attns += (layer_outputs[1],)
 
         hidden_states = self.norm(hidden_states)
-        print("=============================")
+        print_rank_0("=============================")
         print_rank_0("qwen2_5_vl_model_hidden_states_2", hidden_states)
-        print("=============================")
+        print_rank_0("=============================")
 
         # add hidden states from the last decoder layer
         if output_hidden_states:
