6c32ca78e98c500f04675b6f583443452fcbfea0
maosiyang <you@kuaishou.com>
2025-05-12 20:18:17 +0800

    add qwen3

--- recipes/siglip_state_dict_check.py
+++
@@ -1,4 +0,0 @@
-from recovlm.models.qwen_3_vl_2.modeling_qwen2_5_vl import Qwen2_5_VLForConditionalGeneration_siglip
-
-model = Qwen2_5_VLForConditionalGeneration_siglip.from_pretrained("/llm_reco_ssd/zhouyang12/models/Qwen3-8B-Base-siglip")
-
