7134e5cfaeb32904c9e7c0e22d30c1d5793c0ad8
maosiyang <you@kuaishou.com>
2025-05-13 12:59:53 +0800

    add qwen3

--- recipes/test_load_model_2.py
+++ recipes/test_load_model_2.py
@@ -2,7 +2,7 @@ from recovlm.models.qwen_3_vl_2.processing_qwen2_5_vl import Qwen2_5_VLProcessor
 from transformers.models.qwen3.modeling_qwen3 import Qwen3Model
 import json
 # load the tokenizer and the model
-MODEL_DIR="/llm_reco_ssd/zhouyang12/models/Qwen3vl-8B-Base"
+MODEL_DIR="/llm_reco_ssd/zhouyang12/models/Qwen3-8B/Qwen3-8B"
 processor_dir = '"/llm_reco_ssd/zhouyang12/models/Qwen3-8B-Base-siglip"'
 processor = Qwen2_5_VLProcessor_siglip.from_pretrained(processor_dir)
 model = Qwen3Model.from_pretrained(
@@ -32,18 +32,18 @@ inputs = inputs.to(model.device)
 print('inputs', inputs)
 print("input_ids",inputs.input_ids.shape)
 
-# output = model(**inputs)
+output = model(**inputs)
 
-# logits = output.logits
-# print(logits)
+logits = output.logits
+print(logits)
 
 
-generated_ids = model.generate(**inputs, max_new_tokens=128)
-generated_ids_trimmed = [
-        out_ids[len(in_ids) :] for in_ids, out_ids in zip(inputs.input_ids, generated_ids)
-    ]
-output_text = processor.batch_decode(
-    generated_ids_trimmed, skip_special_tokens=True, clean_up_tokenization_spaces=False
-)
-print(output_text)
-#print_rank_0(output)
+# generated_ids = model.generate(**inputs, max_new_tokens=128)
+# generated_ids_trimmed = [
+#         out_ids[len(in_ids) :] for in_ids, out_ids in zip(inputs.input_ids, generated_ids)
+#     ]
+# output_text = processor.batch_decode(
+#     generated_ids_trimmed, skip_special_tokens=True, clean_up_tokenization_spaces=False
+# )
+# print(output_text)
+# #print_rank_0(output)
