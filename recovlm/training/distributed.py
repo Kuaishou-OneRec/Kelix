6b54d0f4078b3dd15cddc0dd5544bc5ae124b6bc
lingzhixin <lingzhixin@kuaishou.com>
2025-05-13 13:06:27 +0000

    email=lingzhixin@kuaishou.com,time=20250513 13:06:27,script=examples/vlm/qwen3navit/debug_qwen3navit_freeze_llm_vit.sh,node=1,comment=version:0.4.1;model_size:72B;GPU_type:H800;data:inner & outer comments,output=/llm_reco/lingzhixin/exps/qwen3navit/debug_qwen3navit/0.0.1/, resume

--- recovlm/training/distributed.py
+++ recovlm/training/distributed.py
@@ -286,7 +286,10 @@ def load_from_full_model_state_dict(model: "FSDPModule", full_sd: Dict[str, Any]
             if allow_random_init_params is not None and k in allow_random_init_params:
                 # full_sd[k] = meta_sharded_sd[k].clone()
                 full_sd[k] = torch.rand(extra_meta_sharded_sd[k][0]) * 0.1 # ) .to(device0)
-                nn.init.kaiming_normal_(full_sd[k], a=0, mode='fan_in', nonlinearity='relu')
+                if full_sd[k].ndim >= 2:
+                    nn.init.kaiming_normal_(full_sd[k], a=0, mode='fan_in', nonlinearity='relu')
+                else:
+                    nn.init.zeros_(full_sd[k])  # 最常见
                 full_sd[k] = full_sd[k].to(device0)
                 # full_sd[k] = meta_sharded_sd[k].clone().to(device0)
                 print(f"random init k={k}, {extra_meta_sharded_sd[k]}\n, meta_sharded_sd={meta_sharded_sd[k]} \nfull={full_sd[k]}")
