c43eb0d9eac65843ada14d441a3400b63f20aed0
liuyang76 <liuyang76@kuaishou.com>
2025-05-13 23:59:44 +0800

    email=liuyang76@kuaishou.com,time=20250513 23:59:43,script=examples/vlm/0.0.0/qwen3navit_1.7B_stage1.sh,node=46,comment=version:0.4.1;model_size:72B;GPU_type:H800;data:inner & outer comments,output=/llm_reco/liuyang76/train_out/0.0.0/qwen3_2B_stage1/, resume

--- examples/vlm/0.0.0/qwen3navit_1.7B_stage1.sh
+++ examples/vlm/0.0.0/qwen3navit_1.7B_stage1.sh
@@ -126,9 +126,9 @@ nohup mpirun --allow-run-as-root \
                 --min_lr 5e-4 \
                 --weight_decay 0.1 \
                 --lr_scheduler_type cosine \
-                --num_warmup_steps 500 \
-                --num_training_steps 5000000 \
-                --save_checkpoint_per_step 2000 \
+                --num_warmup_steps 250 \
+                --num_training_steps 7464 \
+                --save_checkpoint_per_step 1000 \
                 --sequence_parallel_size 1 \
                 --use_flash_attention_2 \
                 --logging_per_step 5 \
