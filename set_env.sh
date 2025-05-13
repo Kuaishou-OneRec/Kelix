24b9ed1bcb2d6f669b8effb02c02575c43abfbe1
liuyang76 <liuyang76@kuaishou.com>
2025-05-13 21:56:14 +0800

    email=liuyang76@kuaishou.com,time=20250513 21:56:14,script=examples/vlm/0.0.0/qwen3navit_1.7B_stage1.sh,node=46,comment=version:0.4.1;model_size:72B;GPU_type:H800;data:inner & outer comments,output=/llm_reco/liuyang76/train_out/0.0.0/qwen3_2B_stage1/, resume

--- set_env.sh
+++ set_env.sh
@@ -31,7 +31,7 @@ echo "Loaded environment variables from ${ENV_FILE}:"
 cat "${ENV_FILE}"
 
 nohup rm -rf hs_err_pid*.log &
-#mpirun --allow-run-as-root --hostfile /etc/mpi/hostfile --pernode bash -c "pip3 install timm==1.0.15"
-#mpirun --allow-run-as-root --hostfile /etc/mpi/hostfile --pernode bash -c "pip3 install fastparquet==2024.2.0"
-#mpirun --allow-run-as-root --hostfile /etc/mpi/hostfile --pernode bash -c "wget https://halo.corp.kuaishou.com/api/cloud-storage/v1/public-objects/user-cloud-storage/xray%2Finstall_xray.sh -O install_xray.sh && bash install_xray.sh"
+mpirun --allow-run-as-root --hostfile /etc/mpi/hostfile --pernode bash -c "pip3 install timm==1.0.15"
+mpirun --allow-run-as-root --hostfile /etc/mpi/hostfile --pernode bash -c "pip3 install fastparquet==2024.2.0"
+mpirun --allow-run-as-root --hostfile /etc/mpi/hostfile --pernode bash -c "wget https://halo.corp.kuaishou.com/api/cloud-storage/v1/public-objects/user-cloud-storage/xray%2Finstall_xray.sh -O install_xray.sh && bash install_xray.sh"
 
