a60deaa337454377919a2ab356b0bac88198c515
maosiyang <you@kuaishou.com>
2025-05-13 11:50:00 +0800

    add qwen3

--- recovlm/utils/merge_weights.py
+++ recovlm/utils/merge_weights.py
@@ -40,15 +40,15 @@ def main():
           if "vision_model" in key:
               pt1[key] = f.get_tensor(key)
   pt2 = {}
-  for i in range(1, 6):
-      with safe_open("/llm_reco_ssd/zhouyang12/models/Qwen3-8B/Qwen3-8B/model-0000" + str(i) + "-of-00005.safetensors", framework="pt", device="cpu") as f:
+  for i in range(1, 3):
+      with safe_open("/llm_reco_ssd/zhouyang12/models/Qwen3-1.7B/Qwen3-1.7B/model-0000" + str(i) + "-of-00002.safetensors", framework="pt", device="cpu") as f:
           for key in f.keys():
               pt2[key] = f.get_tensor(key)
   print('lalallalalallal')
   for key in pt1.keys():
     keypt2 = "visual." + key
     pt2[keypt2] = pt1[key]
-  outputdir = "/llm_reco_ssd/zhouyang12/models/Qwen3-8B-siglip"
+  outputdir = "/llm_reco_ssd/zhouyang12/models/Qwen3-1.7B-siglip"
   os.makedirs(outputdir, exist_ok=True)
   #merge pt1 and pt2
   save_file(pt2, outputdir + "/model.safetensors",metadata={"format": "pt"})
