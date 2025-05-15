14422d2ee5644242b02c6ccc51343f47bcb8b259
lingzhixin <lingzhixin@kuaishou.com>
2025-05-16 00:43:05 +0800

    email=lingzhixin@kuaishou.com,time=20250516 00:43:05,script=examples/vlm/qwen3navit/debug_qwen3navit_0516_8b_stage2.sh,node=1,comment=version:0.4.1;model_size:72B;GPU_type:H800;data:inner & outer comments,output=/llm_reco/lingzhixin/exps/qwen3navit/debug_qwen3navit_0516_8b_stage2/0.0.3/8B/, resume

--- examples/vlm/qwen3navit/debug_qwen3navit_0516_8b_stage2.sh
+++ examples/vlm/qwen3navit/debug_qwen3navit_0516_8b_stage2.sh
@@ -119,7 +119,7 @@ nohup mpirun --allow-run-as-root \
         with_nccl_local_env \
         python3 recipes/train_fsdp.py --model_dir $MODEL_DIR \
                 --output_dir $OUTPUT_DIR \
-                --dataset_config examples/vlm/0.0.0_2B_stage1/qwen3navit_1.7B_stage1.json \
+                --dataset_config examples/vlm/qwen3navit/qwen3navit_1.7B_stage1.json \
                 --model_class Qwen3SiglipForConditionalGeneration_navit \
 		--allow_random_init_params 'mlp_AR.pre_norm.weight,mlp_AR.pre_norm.bias,mlp_AR.linear_1.weight,mlp_AR.linear_1.bias,mlp_AR.linear_2.weight,mlp_AR.linear_2.bias' \
                 --monitor_datasource_loss \
