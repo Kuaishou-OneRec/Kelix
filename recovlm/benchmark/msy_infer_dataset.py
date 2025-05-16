228fa7eacfc111e6000bee8bf8287269638e9d15
maosiyang <you@kuaishou.com>
2025-05-16 16:49:52 +0800

    na

--- recovlm/benchmark/msy_infer_dataset.py
+++ recovlm/benchmark/msy_infer_dataset.py
@@ -42,6 +42,7 @@ def format_text(doc, max_text_len=1000):
 def MMBenchTransform(sample) -> list:
   index = sample['index']
   image = sample['image']
+  print('test',image)
   answer = sample['answer']
   hint = sample['hint'] if sample['hint'] else 'N/A'
   question = sample['question']
