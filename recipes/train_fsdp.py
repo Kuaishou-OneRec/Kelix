e4a7ca943e09058ecdbc4a30dcc2336e347979e0
lingzhixin <lingzhixin@kuaishou.com>
2025-05-16 10:38:13 +0800

    email=lingzhixin@kuaishou.com,time=20250516 10:38:13,script=examples/vlm/qwen3navit/debug_qwen3navit_0516_8b_stage2.sh,node=1,comment=version:0.4.1;model_size:72B;GPU_type:H800;data:inner & outer comments,output=/llm_reco/lingzhixin/exps/qwen3navit/debug_qwen3navit_0516_8b_stage2/0.0.3/8B/, resume

--- recipes/train_fsdp.py
+++ recipes/train_fsdp.py
@@ -1161,7 +1161,7 @@ def train():
 
           avg_loss = acc_avg_loss / args.gradient_accumulation_steps / args.logging_per_step
           mfu_per_step_per_gpu = calc_mfu(os.path.join(args.model_dir, "config.json"), 
-            total_seq_len=acc_num_tokens, 
+            total_seq_len=acc_num_tokens / dist.get_world_size(), 
             image_token_merged_len=[round(acc_num_image_tokens / acc_num_images)] * acc_num_images if acc_num_images != 0 else 1, 
             llm_batch_size=acc_num_samples, 
             secs_per_step=end_time - start_time)
