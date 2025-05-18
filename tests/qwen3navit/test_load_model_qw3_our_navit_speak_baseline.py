1efe795d5928edd05a50c767820730e661ab80a8
凌致新 <lingzhixin@kuaishou.com>
2025-05-18 16:17:42 +0800

    c

--- tests/qwen3navit/test_load_model_qw3_our_navit_speak_baseline.py
+++ tests/qwen3navit/test_load_model_qw3_our_navit_speak_baseline.py
@@ -236,8 +236,7 @@ def make_inputs(a,b):
     text = processor.apply_chat_template(
         messages, tokenize=False, add_generation_prompt=False
     )
-    image_inputs, video_inputs = process_vision_info(messages, #image_factor=None
-                                                     )
+    image_inputs, video_inputs = process_vision_info(messages, image_factor=None)
     print(image_inputs, video_inputs)
     inputs = processor(
         text=[text],
