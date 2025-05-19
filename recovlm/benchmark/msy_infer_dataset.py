fd74af3b00b4ec4f018a11c65dcfd18d97a88bc8
maosiyang <you@kuaishou.com>
2025-05-19 14:16:16 +0800

    na

--- recovlm/benchmark/msy_infer_dataset.py
+++ recovlm/benchmark/msy_infer_dataset.py
@@ -185,7 +185,7 @@ class MsyInferDataset(ParquetDataset):
       print(assistant_responses)#[array(['CENTRE'], dtype=object)]
       start_pos_list = []
       for assistant_response in assistant_responses:
-        print(type(assistant_response))#['CENTRE']
+        assistant_response = assistant_response.tolist()
         text = [assistant_response]
         assistant_tokens = self.processor(
           text=text,
