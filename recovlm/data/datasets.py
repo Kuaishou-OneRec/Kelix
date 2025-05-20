bdb722cc4e57265f54bcde2aefc4ed56ef12f267
lingzhixin <lingzhixin@kuaishou.com>
2025-05-20 17:33:49 +0800

    email=lingzhixin@kuaishou.com,time=20250520 17:33:49,script=examples/vlm/token_bal/intern_stage2_bal.sh,node=1,comment=run_internvl_2b_stage1_by_ccl,output=/llm_reco/lingzhixin/exp_outputs/vlm/token_bal/0.0.1/

--- recovlm/data/datasets.py
+++ recovlm/data/datasets.py
@@ -3800,7 +3800,8 @@ class InternVLChatCompletionVisionDataset(IterableDataset):
                                       cu_seqlens,
                                       sample_idx=-1)
 
-    if self.cut_to_pad: assert valid_seq_len == self.max_length, f"set cut_to_pad={self.cut_to_pad}, then require valid_seq_len/{valid_seq_len} == self.max_length/{self.max_length}"
+    if self.cut_to_pad and not valid_seq_len == self.max_length and np.random.rand() < 0.01: 
+      print(f"intern_data warning: set cut_to_pad={self.cut_to_pad}, then require valid_seq_len/{valid_seq_len} == self.max_length/{self.max_length}")
     packed_input_ids = torch.cat(packed_input_ids, dim=0).unsqueeze(0)
     packed_loss_mask = torch.cat(packed_loss_mask, dim=0).unsqueeze(0)
     packed_position_ids = torch.cat(packed_position_ids, dim=-1)
