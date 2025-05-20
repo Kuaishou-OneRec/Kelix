f9a42e4bc176f1269443baf8cfb8a5cf1ddb7d9b
huangsui <huangsui@kuaishou.com>
2025-05-20 23:08:29 +0800

    email=huangsui@kuaishou.com,time=20250520 23:08:29,script=examples/vlm/qwen3navit/hs_qwen3navit_8B256.sh,node=15,comment=version:0.4.1;model_size:72B;GPU_type:H800;data:inner&outer_comments,output=/llm_reco/huangsui/output/qwen3navit/hs_balance/0.0.2/8B256/, resume

--- recipes/train_fsdp.py
+++ recipes/train_fsdp.py
@@ -1147,6 +1147,8 @@ def train():
 
           ticker.tick(f"optimizer.step*{args.gradient_accumulation_steps}")
 
+      if dist.get_rank() == 0:
+          print(f"current_gpu_memory: {torch.cuda.max_memory_allocated() / 1024 / 1024} MB")
       ########## dataset source monitor ###############
       if args.monitor_datasource_loss:
         # WARN: assume batch_size = 1
