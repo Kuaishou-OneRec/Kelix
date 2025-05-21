7e69dea43962ed810233fa2de669c88f8223d74d
凌致新 <lingzhixin@kuaishou.com>
2025-05-21 16:05:51 +0800

    c

--- recovlm/data/balance.py
+++ recovlm/data/balance.py
@@ -202,10 +202,10 @@ class CustomModelFlops(ModelFlopsBase):
         super(CustomModelFlops, self).__init__(flops_range)
 
     def llm_flops(self, seq_list: List[int]) -> float:
-        return calculate_llm_flops_from_config(self.base_model_config, seq_list, None) / 1e12
+        return calculate_llm_flops_from_config(self.base_model_config, seq_list, None)['total_flops'] / 1e12
 
     def vit_flops(self, image_list: List[int]) -> float:
-        return calculate_llm_flops_from_config(self.base_model_config, image_list, None) / 1e12
+        return calculate_llm_flops_from_config(self.base_model_config, image_list, None)['total_flops'] / 1e12
 
 
 def flops_diff(flops1, flops2):
