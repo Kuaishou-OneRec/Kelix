9347d10a802fa80a8dd7a3567c2a1c95de818848
lingzhixin <lingzhixin@kuaishou.com>
2025-05-22 12:09:18 +0800

    email=lingzhixin@kuaishou.com,time=20250522 12:09:18,script=examples/vlm/debug_internvl3_2b_hs.sh,node=1,comment=run_internvl_2b_stage1_by_ccl,output=/llm_reco/huangsui/output/internvl-2b/hs_balance_llm_flops3

--- examples/vlm/debug_internvl3_2b_hs.sh
+++ examples/vlm/debug_internvl3_2b_hs.sh
@@ -17,7 +17,7 @@ sed 's/=1/=8/g' /etc/mpi/hostfile  | head -1999  > /etc/mpi/hostfile_seq
 # MODEL_DIR=/llm_reco_ssd/luoxinchen/output/RecoVLM/Qwen2-VL-7B-stage1-v0.0.36/global_step90000-hf
 MODEL_DIR=/llm_reco/chuchenglong/InternVL/models/Megred_model/2B # Pretrained/Base model path
 # MODEL_DIR=/llm_reco/chuchenglong/InternVL/models/OpenGVLab/InternVL2_5-4B
-OUTPUT_DIR=/llm_reco/huangsui/output/internvl-2b/hs_balance_llm_flops
+OUTPUT_DIR=/llm_reco/huangsui/output/internvl-2b/hs_balance_llm_flops3
 rm -rf $OUTPUT_DIR
 mkdir -p $OUTPUT_DIR
 
