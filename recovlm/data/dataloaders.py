7d655cfcc2e7f6decb59ec0797447ebe521777d4
lingzhixin <lingzhixin@kuaishou.com>
2025-05-22 20:17:52 +0800

    email=lingzhixin@kuaishou.com,time=20250522 20:17:52,script=examples/vlm/keye/debug_keye_8B256_bal.sh,node=1,comment=xxx,output=/llm_reco/lingzhixin/exps/keye/debug_Keye-8B-scratch_bal/0.0.1/8B256/, resume

--- recovlm/data/dataloaders.py
+++ recovlm/data/dataloaders.py
@@ -260,7 +260,7 @@ def get_chat_completion_vision_parquet_dataloader(sources: str,
                     'Qwen3SiglipForConditionalGeneration_navit':ChatCompletionVisionParquetDataset_navit,
                     'KeyeForConditionalGeneration': ChatCompletionVisionParquetDataset_keye,
                     'InternVLChatModel':InternVLChatCompletionVisionParquetDataset}
-    assert ChatCompletionVisionParquetDataset_keye == ModelDataset
+    assert ChatCompletionVisionParquetDataset_keye == model_type
     num_readers = kwargs.get("num_readers", 1)
     shuffle_window = kwargs.get("shuffle_window", 0)
     if use_balance:
