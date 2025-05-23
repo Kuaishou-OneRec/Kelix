b5f188d78979ffd9a0e11ad0fbeca731c8b5df79
lingzhixin <lingzhixin@kuaishou.com>
2025-05-23 16:49:17 +0800

    c

--- examples/vlm/0.8.0/qwen3navit_2B_stage3.210_fix.sh
+++ examples/vlm/0.8.0/qwen3navit_2B_stage3.210_fix.sh
@@ -16,7 +16,7 @@ sed 's/=1/=8/g' /etc/mpi/hostfile > /etc/mpi/hostfile_seq
 
 # MODEL_DIR=/llm_reco_ssd/luoxinchen/output/RecoVLM/Qwen2-VL-7B-stage1-v0.0.36/global_step90000-hf
 MODEL_DIR=/llm_reco_ssd/zhouyang12/models/Qwen3-1.7B-siglip/
-OUTPUT_DIR=/mmu_mllm_hdd_2/zhouyang12/output/Keye/Stage3_data_0.3.2/0.8.0/209/2b/
+OUTPUT_DIR=/mmu_mllm_hdd_2/zhouyang12/output/Keye/Stage3_data_0.3.2/0.8.0/210/2b/
 
 mkdir -p $OUTPUT_DIR
 
@@ -116,7 +116,7 @@ nohup mpirun --allow-run-as-root \
         with_nccl_local_env \
         bash -c "bash numa_runner.sh python3 recipes/train_fsdp.py --model_dir $MODEL_DIR \
                 --output_dir $OUTPUT_DIR \
-                --dataset_config examples/vlm/0.8.0/qwen3navit_2B_stage3.209.json \
+                --dataset_config examples/vlm/0.8.0/qwen3navit_2B_stage3.210.json \
                 --model_class Qwen3SiglipForConditionalGeneration_navit \
                 --allow_random_init_params 'mlp_AR.pre_norm.weight,mlp_AR.pre_norm.bias,mlp_AR.linear_1.weight,mlp_AR.linear_1.bias,mlp_AR.linear_2.weight,mlp_AR.linear_2.bias' \
                 --monitor_datasource_loss \
@@ -128,7 +128,7 @@ nohup mpirun --allow-run-as-root \
                 --weight_decay 0.1 \
                 --lr_scheduler_type cosine \
                 --num_warmup_steps 250 \
-                --num_training_steps 4500 \
+                --num_training_steps 8500 \
                 --save_checkpoint_per_step 500 \
                 --sequence_parallel_size 1 \
                 --use_flash_attention_2 \
