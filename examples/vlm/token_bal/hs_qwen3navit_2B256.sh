3497490e91fc549d50c807d582e23c2ace234b55
huangsui <huangsui@kuaishou.com>
2025-05-23 17:32:13 +0800

    email=huangsui@kuaishou.com,time=20250523 17:32:13,script=examples/vlm/token_bal/hs_qwen3navit_2B256.sh,node=11,comment=version:0.4.1;model_size:72B;GPU_type:H800;data:inner&outer_comments,output=/llm_reco/huangsui/output/qwen3navit/hs_balance_gpu88_oom/0.0.1/2B256/, resume

--- examples/vlm/token_bal/hs_qwen3navit_2B256.sh
+++ examples/vlm/token_bal/hs_qwen3navit_2B256.sh
@@ -121,7 +121,7 @@ nohup mpirun --allow-run-as-root \
 		        --allow_random_init_params 'mlp_AR.pre_norm.weight,mlp_AR.pre_norm.bias,mlp_AR.linear_1.weight,mlp_AR.linear_1.bias,mlp_AR.linear_2.weight,mlp_AR.linear_2.bias' \
                 --monitor_datasource_loss \
                 --monitor_datasource_cnt \
-                --max_length 22000 \
+                --max_length 25000 \
                 --learning_rate 1e-6 \
                 --min_lr 0.0 \
                 --weight_decay 0.1 \
