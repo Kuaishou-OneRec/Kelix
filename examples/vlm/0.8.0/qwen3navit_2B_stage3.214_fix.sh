77d7d6591d250f9141a5d1f317cbfd2f6f41d06f
lingzhixin <lingzhixin@kuaishou.com>
2025-05-25 03:35:26 +0800

    email=lingzhixin@kuaishou.com,time=20250525 03:35:26,script=examples/vlm/0.8.0/qwen3navit_2B_stage3.213_fix.sh,node=16,comment=128H_080_201,output=/mmu_mllm_hdd_2/zhouyang12/output/Keye/Stage3_data_0.3.2/0.8.0/213/2b/, resume

--- examples/vlm/0.8.0/qwen3navit_2B_stage3.214_fix.sh
+++ examples/vlm/0.8.0/qwen3navit_2B_stage3.214_fix.sh
@@ -16,7 +16,7 @@ sed 's/=1/=8/g' /etc/mpi/hostfile > /etc/mpi/hostfile_seq
 
 # MODEL_DIR=/llm_reco_ssd/luoxinchen/output/RecoVLM/Qwen2-VL-7B-stage1-v0.0.36/global_step90000-hf
 MODEL_DIR=/llm_reco_ssd/zhouyang12/models/Qwen3-1.7B-siglip/
-OUTPUT_DIR=/mmu_mllm_hdd_2/zhouyang12/output/Keye/Stage3_data_0.3.2/0.8.0/213/2b/
+OUTPUT_DIR=/mmu_mllm_hdd_2/zhouyang12/output/Keye/Stage3_data_0.3.2/0.8.0/206/2b/
 
 mkdir -p $OUTPUT_DIR
 
@@ -116,7 +116,7 @@ nohup mpirun --allow-run-as-root \
         with_nccl_local_env \
         bash -c "bash numa_runner.sh python3 recipes/train_fsdp.py --model_dir $MODEL_DIR \
                 --output_dir $OUTPUT_DIR \
-                --dataset_config examples/vlm/0.8.0/qwen3navit_2B_stage3.213.json \
+                --dataset_config examples/vlm/0.8.0/qwen3navit_2B_stage3.206.json \
                 --model_class Qwen3SiglipForConditionalGeneration_navit \
                 --allow_random_init_params 'mlp_AR.pre_norm.weight,mlp_AR.pre_norm.bias,mlp_AR.linear_1.weight,mlp_AR.linear_1.bias,mlp_AR.linear_2.weight,mlp_AR.linear_2.bias' \
                 --monitor_datasource_loss \
@@ -144,7 +144,7 @@ nohup mpirun --allow-run-as-root \
                 --commit_id $git_hash \
                 --kml_id $KML_ID \
                 --kml_task_id $KML_TASK_ID \
-                --resume_from /mmu_mllm_hdd_2/zhouyang12/output/Keye/Stage2_resume_1k/0.8.0/2b/step5000 \
-		--resume_from_tag global_step5000 \
+                --resume_from /mmu_mllm_hdd_2/zhouyang12/output/Keye/Stage2_0.4.2_resume10k/0.8.0/2b/step2000 \
+		--resume_from_tag global_step2000 \
                 --heartbeat_monitor" > $OUTPUT_DIR/stdout.log 2>$OUTPUT_DIR/stderr.log &
 
