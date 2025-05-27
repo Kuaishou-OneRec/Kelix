7eb70a1e602781354b99ed71b2a27bde88d8ac39
lingzhixin <lingzhixin@kuaishou.com>
2025-05-27 14:47:58 +0800

    email=lingzhixin@kuaishou.com,time=20250527 14:47:58,script=examples/vlm/sp_debug_0527/keye_8B_stage2_0405sp.sh,node=1,comment=128H_080_201,output=/mmu_mllm_hdd_2/lingzhixin/output/Keye/spdebug_sp4/0.0.1/8b, resume

--- examples/vlm/sp_debug_0527/keye_8B_stage2_0405sp_video.sh
+++ examples/vlm/sp_debug_0527/keye_8B_stage2_0405sp_video.sh
@@ -117,13 +117,13 @@ nohup mpirun --allow-run-as-root \
         with_nccl_local_env \
         bash -c "bash numa_runner.sh python3 recipes/train_fsdp.py --model_dir $MODEL_DIR \
                 --output_dir $OUTPUT_DIR \
-                --dataset_config examples/vlm/sp_debug_0527/keye_8B_stage2_0405sp.json \
+                --dataset_config examples/vlm/sp_debug_0527/keye_8B_stage2_0405sp_video.json \
                 --model_class KeyeForConditionalGeneration \
                 --allow_random_init_params 'mlp_AR.pre_norm.weight,mlp_AR.pre_norm.bias,mlp_AR.linear_1.weight,mlp_AR.linear_1.bias,mlp_AR.linear_2.weight,mlp_AR.linear_2.bias' \
                 --monitor_datasource_loss \
                 --monitor_datasource_cnt \
 		--monitor_image_tokens \
-                --max_length 40000 \
+                --max_length 36000 \
                 --learning_rate 1e-5 \
                 --vision_learning_rate 1e-6 \
                 --min_lr 1e-6 \
