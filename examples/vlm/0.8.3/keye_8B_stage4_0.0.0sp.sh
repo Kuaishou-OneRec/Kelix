7b1bf3df563c0c7eb67fbd372ab046f99c10e8ac
lingzhixin <lingzhixin@kuaishou.com>
2025-05-27 23:35:17 +0800

    email=lingzhixin@kuaishou.com,time=20250527 23:35:16,script=examples/vlm/0.8.3/keye_8B_stage4_0.0.0sp.sh,node=3,comment=24卡Stage4 长文本比例不足,output=/mmu_mllm_hdd_2/zhouyang12/output/Keye/Stage4_0.0.0/0.8.0/8B/, resume

--- examples/vlm/0.8.3/keye_8B_stage4_0.0.0sp.sh
+++ examples/vlm/0.8.3/keye_8B_stage4_0.0.0sp.sh
@@ -129,7 +129,7 @@ nohup mpirun --allow-run-as-root \
                 --min_lr 1e-6 \
                 --weight_decay 0.1 \
                 --lr_scheduler_type cosine \
-                --num_warmup_steps 500 \
+                --num_warmup_steps 2500 \
                 --num_training_steps  \
                 --save_checkpoint_per_step 35000 \
                 --sequence_parallel_size 4 \
