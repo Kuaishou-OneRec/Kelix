e7ea3d7b2ddc4db59b05be221a54c71237d0da1a
lingzhixin <lingzhixin@kuaishou.com>
2025-05-27 16:26:22 +0800

    email=lingzhixin@kuaishou.com,time=20250527 16:26:22,script=examples/vlm/sp_debug_0527/keye_8B_stage2_0405sp.sh,node=1,comment=128H_080_201,output=/mmu_mllm_hdd_2/lingzhixin/output/Keye/spdebug_sp4/0.0.2/8b, resume

--- recovlm/models/keye/modeling_keye.py
+++ recovlm/models/keye/modeling_keye.py
@@ -1147,7 +1147,7 @@ class SiglipEncoder(nn.Module):
         attention_mask = attention_mask.to(inputs_embeds.dtype) if attention_mask is not None else None
 
         hidden_states = get_local_sequence(hidden_states, seq_idx=1)
-        rotary_pos_emb = get_local_sequence(rotary_pos_emb, seq_idx=1)
+        # rotary_pos_emb = get_local_sequence(rotary_pos_emb, seq_idx=1)
 
         for encoder_layer in self.layers:
             if output_hidden_states:
