ce5fd54a8adc8b8e8e7e9533bff3d556db368326
lingzhixin <lingzhixin@kuaishou.com>
2025-05-28 11:25:05 +0800

    email=lingzhixin@kuaishou.com,time=20250528 11:25:05,script=examples/vlm/0.8.3/keye_8B_stage4_0.0.0sp.sh,node=1,comment=24H800Stage4,output=/mmu_mllm_hdd_2/lingzhixin/output/Keye/Stage4_0.0.0/0.8.0d/8B/, resume

--- recipes/train_fsdp.py
+++ recipes/train_fsdp.py
@@ -963,6 +963,8 @@ def train():
   token_stasts = TokenStats(args)
 
   gpu_batch_q = queue.Queue(maxsize=2)
+
+
   def prefetch_to_gpu(input_fn, output_q, dev):
     while True:
       try:
@@ -977,8 +979,8 @@ def train():
   else:
     data_iter = iter(gather_by_group(dataloader, get_sequence_parallel_group()))
     input_fn =  lambda: next(data_iter)
-  # prefetch_t = threading.Thread(target=prefetch_to_gpu, args=(input_fn, gpu_batch_q, torch.cuda.current_device()))
-  # prefetch_t.start()
+  prefetch_t = threading.Thread(target=prefetch_to_gpu, args=(input_fn, gpu_batch_q, torch.cuda.current_device()))
+  prefetch_t.start()
 
   tb_metrics_q = queue.Queue(maxsize=8)
   def write_tb_async(tb_writer, metrics_queue, grad_acc_steps):
@@ -1042,7 +1044,7 @@ def train():
       if torch_profiler: ctx.enter_context(torch_profiler)
 
       ticker.tick("enter_context(torch_profiler)")
-      try: batch = batch_queue.get() if batch_queue is not None else input_fn() 
+      try: batch = gpu_batch_q.get() # input_fn() 
       except StopIteration: break
       ticker.tick("next_batch")
       
