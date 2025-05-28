93625e021fa2c1a389f5c0224438891a7ebabf5c
lingzhixin <lingzhixin@kuaishou.com>
2025-05-28 20:02:24 +0800

    email=lingzhixin@kuaishou.com,time=20250528 20:02:24,script=examples/vlm/0.8.2/keye_8B_stage2_0405.sh,node=64,comment=128H_080_201,output=/llm_reco/lingzhixin/output/Keye/Stage2_0.4.5/0.8.2/8b, resume

--- recovlm/data/datasets.py
+++ recovlm/data/datasets.py
@@ -966,7 +966,7 @@ class ChatCompletionVisionDataset(IterableDataset):
         videos=video_inputs,
         return_tensors="pt"
     )
-    if time.time() - time0 > 1:
+    if time.time() - time0 > 2:
       print(f"long process time source={sample['json']['source']}, it consumes {time.time() - time0} secs", )
 
     # For the Warning: (add by zzx)
@@ -1058,7 +1058,7 @@ class ChatCompletionVisionDataset(IterableDataset):
         videos=video_inputs,
         return_tensors="pt"
     )
-    if time.time() - time0 > 1: 
+    if time.time() - time0 > 2: 
       print(f"long process time source={sample['json']['source']}, it consumes {time.time() - time0} secs", )
 
     # For the Warning: (add by zzx)
