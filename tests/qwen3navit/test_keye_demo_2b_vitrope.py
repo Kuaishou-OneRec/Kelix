b184bb02753689e5f8bdda0db5cec497b6b765c1
maosiyang <you@kuaishou.com>
2025-06-04 20:11:34 +0800

    add keyevitrope

--- tests/qwen3navit/test_keye_demo_2b_vitrope.py
+++ tests/qwen3navit/test_keye_demo_2b_vitrope.py
@@ -156,7 +156,7 @@ if 1:
             )
 
 
-            messages, inputs = make_inputs(200,200)
+            messages, inputs = make_inputs(56,56)
             for k in inputs: inputs[k] = inputs[k].cuda()
 
             generated = model.generate(**inputs, max_new_tokens=255)
