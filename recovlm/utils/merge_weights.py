da3cda5bdcd8e819a0beabcb8a751ee8e7c7cd0e
maosiyang <you@kuaishou.com>
2025-06-04 15:37:09 +0800

    add keyevitrope

--- recovlm/utils/merge_weights.py
+++ recovlm/utils/merge_weights.py
@@ -1,10 +1,7 @@
 import argparse
 import re
 import os
-from recovlm.models.qwen_3_vl_2.modeling_qwen2_5_vl import Qwen2_5_VLForConditionalGeneration_siglip
-from recovlm.models.qwen_3_vl_2.configuration_qwen2_5_vl import Qwen2_5_VLConfig
-from recovlm.models.qwen_3_vl_2.modeling_qwen2_5_vl import Qwen2_5_VLModel
-from recipes.ViT.training.models.siglip.modeling_siglip import SiglipVisionModel
+
 import torch
 import transformers
 from safetensors import safe_open
@@ -29,7 +26,7 @@ from safetensors.torch import save_file
 
 def main():
   # Load the PyTorch model file
-  model_path = "/llm_reco_ssd/zangdunju/output2/RecoVLM/SigLIP/siglip_navit/global_step1000/model_float32.pth"
+  model_path = "/mmu_mllm_hdd_2/zangdunju/ckpt/global_step18200/vision_model.pth"
   ptm = torch.load(model_path, map_location='cpu')
   pt1 = {}
   # Print the keys in the state dict
@@ -43,14 +40,14 @@ def main():
   #   print(pt1[key].shape)
   #   print("================================================")
   pt2 = {}
-  for i in range(1, 6):
-      with safe_open("/llm_reco_ssd/zhouyang12/models/Qwen3-8B-Base/model-0000" + str(i) + "-of-00005.safetensors", framework="pt", device="cpu") as f:
+  for i in range(1, 3):
+      with safe_open("/llm_reco_ssd/zhouyang12/models/Qwen3-1.7B/model-0000" + str(i) + "-of-00002.safetensors", framework="pt", device="cpu") as f:
           for key in f.keys():
               pt2[key] = f.get_tensor(key)
   print('lalallalalallal')
   for key in pt1.keys():
     pt2[key] = pt1[key]
-  outputdir = "/llm_reco_ssd/zhouyang12/models/Qwen3-8B-Base-siglip"
+  outputdir = "/llm_reco_ssd/zhouyang12/models/Keye-2B_vitrope_0604"
   os.makedirs(outputdir, exist_ok=True)
   #merge pt1 and pt2
   save_file(pt2, outputdir + "/model.safetensors",metadata={"format": "pt"})
