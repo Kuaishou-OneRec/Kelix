7645afb0585fec5308a4f39fc8c7c08995a36da3
maosiyang <you@kuaishou.com>
2025-06-05 18:06:21 +0800

    add stage2

--- examples/vlm/0.9.0_2B_stage2/qwen3navit_1.7B_stage2.sh
+++ examples/vlm/0.9.0_2B_stage2/qwen3navit_1.7B_stage2.sh
@@ -121,14 +121,14 @@ nohup mpirun --allow-run-as-root \
                 --monitor_datasource_loss \
                 --monitor_datasource_cnt \
                 --max_length 21000 \
-                --learning_rate 4e-6 \
-                --vision_learning_rate 2e-6 \
-                --min_lr 2e-6 \
+                --learning_rate 1e-5 \
+                --vision_learning_rate 1e-6 \
+                --min_lr 5e-7 \
                 --weight_decay 0.1 \
                 --lr_scheduler_type cosine \
-                --num_warmup_steps 300 \
-                --num_training_steps 24000 \
-                --save_checkpoint_per_step 1000 \
+                --num_warmup_steps 1000 \
+                --num_training_steps 9510 \
+                --save_checkpoint_per_step 500 \
                 --sequence_parallel_size 1 \
                 --use_flash_attention_2 \
                 --logging_per_step 20 \
