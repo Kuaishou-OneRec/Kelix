f692109438699cc629fe0bdef8d596b7d338d42c
caojiangxia <caojiangxia@kuaishou.com>
2025-06-08 16:22:28 +0800

    email=caojiangxia@kuaishou.com,time=20250608 16:22:28,script=examples/vlm/keye/debug_keye_8Bslowfast.sh,node=1,comment=slowfast_8B_test,output=/llm_reco_ssd/caojiangxia/output2/RecoVLM/Keye/2.0.0.1, resume

--- examples/vlm/keye/debug_keye_8Bslowfast.sh
+++ examples/vlm/keye/debug_keye_8Bslowfast.sh
@@ -25,7 +25,7 @@ mkdir -p /tmp/_wids_cache
 nnode=$(wc -l < /etc/mpi/hostfile_seq)
 
 # 注意修改实验内容备注
-comment="slowfast 8B test"
+comment="slowfast_8B_test"
 
 git add --all
 git commit -m "email=$email,time=$(date +"%Y%m%d %H:%M:%S"),script=$0,node=$nnode,comment=$comment,output=$OUTPUT_DIR, resume"
