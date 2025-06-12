54242cca19ff77bad2c0795e9e7fed96f55648a3
caojiangxia <caojiangxia@kuaishou.com>
2025-06-12 14:17:32 +0800

    email=caojiangxia@kuaishou.com,time=20250612 14:17:31,script=examples/vlm/keye/debug_keye_8B_vitrope_debug_cjx.sh,node=2,comment=slowfast_8B_test,output=/llm_reco_ssd/caojiangxia/output2/RecoVLM/Keye/2.0.1.1, resume

--- examples/vlm/keye/debug_keye_8B_vitrope_debug_cjx.sh
+++ examples/vlm/keye/debug_keye_8B_vitrope_debug_cjx.sh
@@ -15,7 +15,7 @@ fi
 sed 's/=1/=8/g' /etc/mpi/hostfile > /etc/mpi/hostfile_seq
 
 # MODEL_DIR=/llm_reco_ssd/luoxinchen/output/RecoVLM/Qwen2-VL-7B-stage1-v0.0.36/global_step90000-hf
-MODEL_DIR=/llm_reco_ssd/zhouyang12/models/Keye-8B-demo_hf_vit_rope
+MODEL_DIR=/llm_reco_ssd/zhouyang12/models/Keye-8B-demo_hf_vit_rope_slowfast_0608_split/
 OUTPUT_DIR=/llm_reco_ssd/caojiangxia/output2/RecoVLM/Keye/2.0.1.1
 
 mkdir -p $OUTPUT_DIR
