a815fea5c401fe5bc4f724de7a73de17d30b328a
caojiangxia <caojiangxia@kuaishou.com>
2025-06-24 16:18:31 +0800

    email=caojiangxia@kuaishou.com,time=20250624 16:18:30,script=examples/vlm/0.9.0_8B_stage2_slowfast/keye_0.9.1_stage2_0.4.8_5e-5_sharevit-vl-fix_0624_only_slow_new_vl.sh,node=8,comment=version:0.9.1;model_size:8B:H800:384;Stage2:0.4.8,slowfast,vl-fix,sharevit,slow64frams,output=/mmu_mllm_hdd_2/zhouyang12/output1/Keye/0.9.1/Stage2/8b_slowfast_debug/slowfast-video-0.2.0-sharevit-vl-fix-0624-only_slow-only_slow-new-vl, resume

--- recovlm/models/keye_vitrope_slowfast/keye_vl_utils.py
+++ recovlm/models/keye_vitrope_slowfast/keye_vl_utils.py
@@ -190,67 +190,6 @@ def smart_nframes(
     return nframes, fps_ratio
 
 
-def smart_nframes_slowfast(
-        ele: dict,
-        slow_frames: int,
-        total_frames: int,
-        video_fps: int | float,
-) -> int:
-    """calculate the number of frames for video used for model inputs.
-
-    Args:
-        ele (dict): a dict contains the configuration of video.
-            support either `fps` or `nframes`:
-                - nframes: the number of frames to extract for model inputs.
-                - fps: the fps to extract frames for model inputs.
-                    - min_frames: the minimum number of frames of the video, only used when fps is provided.
-                    - max_frames: the maximum number of frames of the video, only used when fps is provided.
-        total_frames (int): the original total number of frames of the video.
-        video_fps (int | float): the original fps of the video.
-
-    Raises:
-        ValueError: nframes should in interval [FRAME_FACTOR, total_frames].
-
-    Returns:
-        int: the number of frames for video used for model inputs.
-    """
-    assert not ("fps" in ele and "nframes" in ele), "Only accept either `fps` or `nframes`"
-    if "nframes" in ele:
-        nframes = round_by_factor(ele["nframes"], FRAME_FACTOR)
-    else:
-        fps = ele.get("fps", FPS)
-        min_frames = ceil_by_factor(ele.get("min_frames", FPS_MIN_FRAMES), FRAME_FACTOR)
-        if total_frames > ele.get("max_frames", FPS_MAX_FRAMES) * 5:
-            max_frames = ele.get("max_frames", FPS_MAX_FRAMES) * 5
-            slowfast_rate = 5
-        elif total_frames < ele.get("max_frames", FPS_MAX_FRAMES) * 1.5:
-            max_frames = total_frames
-            slowfast_rate = 1
-        else:
-            max_frames = ele.get("max_frames", FPS_MAX_FRAMES) * (total_frames // ele.get("max_frames", FPS_MAX_FRAMES))
-            slowfast_rate = total_frames // ele.get("max_frames", FPS_MAX_FRAMES)
-        # max_frames = floor_by_factor(ele.get("max_frames", min(FPS_MAX_FRAMES, total_frames)), FRAME_FACTOR)
-
-
-        nframes = total_frames / video_fps * fps
-        if nframes < 1:
-            nframes = FRAME_FACTOR
-            slowfast_rate = 1
-        # nframes = min(max(nframes, min_frames), max_frames)
-
-        if nframes >= max_frames:
-            fps_ratio = max_frames / nframes
-            nframes = max_frames
-        else:
-            fps_ratio = 1.0
-        nframes = round_by_factor(nframes, FRAME_FACTOR)
-
-    if not (FRAME_FACTOR <= nframes and nframes <= total_frames):
-        raise ValueError(f"nframes should in interval [{FRAME_FACTOR}, {total_frames}], but got {nframes}, max_frames {max_frames}, video_fps {video_fps}.")
-    return nframes, slowfast_rate, fps_ratio
-
-
-
 def _read_video_torchvision(
         ele: dict,
 ) -> tuple[torch.Tensor, float]:
@@ -373,12 +312,10 @@ def _read_video_decord_slowfast(
     total_frames, video_fps = len(vr), vr.get_avg_fps()
     logger.info(f"decord:  {video_path=}, {total_frames=}, {video_fps=}, time={time.time() - st:.3f}s")
 
-
     nframes, fps_ratio = smart_nframes(ele, total_frames=total_frames, video_fps=video_fps)
     idx = torch.linspace(0, total_frames - 1, nframes).round().long().tolist()
     slow_frames = vr.get_batch(idx).asnumpy()
     slow_frames = torch.tensor(slow_frames).permute(0, 3, 1, 2)  # Convert to TCHW format
-
 
     if total_frames >= nframes * 5:
         fast_nframes = nframes * 5
@@ -395,7 +332,6 @@ def _read_video_decord_slowfast(
     fast_frames = torch.tensor(fast_frames).permute(0, 3, 1, 2)
     fps_ratio *= slowfast_rate
     return slow_frames, fast_frames, slowfast_rate, fps_ratio
-
 
 VIDEO_READER_BACKENDS = {
     "decord": _read_video_decord,
