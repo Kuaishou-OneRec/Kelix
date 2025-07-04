6424aa1b55c22a42f94b2b615124081947fdc827
caojiangxia <caojiangxia@kuaishou.com>
2025-07-04 14:38:41 +0800

    email=caojiangxia@kuaishou.com,time=20250704 14:38:41,script=examples/vlm/0.9.0_8B_stage2_slowfast/debug/keye_0.9.1_stage2_0.4.8_5e-5_sharevit.sh,node=1,comment=version:0.9.1;model_size:8B:H800:384;Stage2:0.4.8,slowfast,output=/mmu_mllm_hdd_2/zhouyang12/output1/Keye/0.9.1/Stage2/8b/slowfast-video-0.3.0-sharevit-0704, resume

--- recovlm/models/keye_vitrope_slowfast/modeling_keye.py
+++ recovlm/models/keye_vitrope_slowfast/modeling_keye.py
@@ -2805,8 +2805,8 @@ class KeyeForConditionalGeneration(Qwen3PreTrainedModel, GenerationMixin):
         self.mlp_AR = Projector(config, config.vision_config)
         self.visual = SiglipVisionModel(config.vision_config)
 
-        self.visual_fast = SiglipVisionModel(config.fast_vision_config)
-        self.fast_mlp_AR = Projector(config, config.fast_vision_config)
+        # self.visual_fast = SiglipVisionModel(config.fast_vision_config)
+        # self.fast_mlp_AR = Projector(config, config.fast_vision_config)
 
         self.model = Qwen3Model(config)
         self.vocab_size = config.vocab_size
