d1c94b31f5506fc2194373a07143083db28c69b4
caojiangxia <caojiangxia@kuaishou.com>
2025-07-08 11:41:02 +0800

    email=caojiangxia@kuaishou.com,time=20250708 11:41:02,script=examples/vlm/0.9.0_8B_stage2_slowfast/keye_0.9.1_stage2_0.4.8_5e-5_slowfast_250705_balance_false.sh,node=63,comment=version:0.9.1;model_size:8B:H800:512;Stage2:0.4.8,output=/mmu_mllm_hdd_2/zhouyang12/output1/Keye/0.9.1/Stage2_5e-5/8b/slowfast_0704_sharevit_only_video_256_balance_false_ml15_test_0708, resume

--- examples/vlm/0.9.0_8B_stage2_slowfast/keye_0.9.1_stage2_0.4.8_5e-5_slowfast_250705_balance_false.sh
+++ examples/vlm/0.9.0_8B_stage2_slowfast/keye_0.9.1_stage2_0.4.8_5e-5_slowfast_250705_balance_false.sh
@@ -17,7 +17,7 @@ sed 's/=1/=8/g' /etc/mpi/hostfile > /etc/mpi/hostfile_seq
 # MODEL_DIR=/llm_reco_ssd/luoxinchen/output/RecoVLM/Qwen2-VL-7B-stage1-v0.0.36/global_step90000-hf
 MODEL_DIR=/llm_reco_ssd/zhouyang12/models/Keye-8B-demo_hf_vit_rope_slowfast_0703_patch14
 #OUTPUT_DIR=/llm_reco/maosiyang/train_out/0.9.1/keye_2B_stage1/
-OUTPUT_DIR=/mmu_mllm_hdd_2/zhouyang12/output1/Keye/0.9.1/Stage2_5e-5/8b/slowfast_0704_sharevit_only_video_256_balance_false_ml15
+OUTPUT_DIR=/mmu_mllm_hdd_2/zhouyang12/output1/Keye/0.9.1/Stage2_5e-5/8b/slowfast_0704_sharevit_only_video_256_balance_false_ml15_test_0708
 
 mkdir -p $OUTPUT_DIR
 
