1aa0a38d4e372e1548992a3ff04e92ec7c706fbf
caojiangxia <caojiangxia@kuaishou.com>
2025-07-08 17:58:42 +0800

    email=caojiangxia@kuaishou.com,time=20250708 17:58:42,script=examples/vlm/0.9.0_8B_stage2_slowfast/keye_0.9.1_stage2_0.4.8_5e-5_slowfast_250705_balance_false_0707.sh,node=61,comment=version:0.9.3;model_size:8B:H800:512;Stage2:0.5.0,sharevit,15000seqlen,output=/mmu_mllm_hdd_2/zhouyang12/output1/Keye/0.9.1/Stage2_5e-5/8b/slowfast_0704_sharevit_only_video_256_balance_false_512_0708_debug, resume

--- recipes/train_fsdp.py
+++ recipes/train_fsdp.py
@@ -1108,7 +1108,7 @@ def train():
       to_cuda(batch)
       ticker.tick("to_cuda(batch)")
       #rint(f"X=1, rank={dist.get_rank()} current_gpu_memory: {torch.cuda.max_memory_allocated() / 1024 / 1024} MB")
-      print(data_source)
+      print("micro_step{}, data_source{}.".format(micro_step, data_source))
       continue
 
       input_ids = batch["input_ids"]
