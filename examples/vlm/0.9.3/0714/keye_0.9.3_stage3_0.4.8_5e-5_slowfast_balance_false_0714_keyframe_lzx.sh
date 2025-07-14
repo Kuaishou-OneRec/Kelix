5c65cbe69e3eff6f565689b19dbfd01a8fe36975
caojiangxia <caojiangxia@kuaishou.com>
2025-07-14 21:33:25 +0800

    email=caojiangxia@kuaishou.com,time=20250714 21:33:25,script=examples/vlm/0.9.3/0714/keye_0.9.3_stage3_0.4.8_5e-5_slowfast_balance_false_0714_keyframe_lzx.sh,node=16,comment=version:0.9.3;model_size:8B:H800:512;Stage3:0.5.0,sharevit,15000seqlen,output=/mmu_mllm_hdd_2/zhouyang12/output1/Keye/0.9.1/Stage3_SlowFast/8b/slowfast_0714_keyframe_lzx, resume

--- examples/vlm/0.9.3/0714/keye_0.9.3_stage3_0.4.8_5e-5_slowfast_balance_false_0714_keyframe_lzx.sh
+++ examples/vlm/0.9.3/0714/keye_0.9.3_stage3_0.4.8_5e-5_slowfast_balance_false_0714_keyframe_lzx.sh
@@ -128,8 +128,8 @@ nohup mpirun --allow-run-as-root \
                 --min_lr 5e-5 \
                 --weight_decay 0.1 \
                 --lr_scheduler_type cosine \
-                --num_warmup_steps 500 \
-                --num_training_steps 5100 \
+                --num_warmup_steps 2100 \
+                --num_training_steps 21000 \
                 --save_checkpoint_per_step 1000 \
                 --sequence_parallel_size 4 \
                 --use_flash_attention_2 \
