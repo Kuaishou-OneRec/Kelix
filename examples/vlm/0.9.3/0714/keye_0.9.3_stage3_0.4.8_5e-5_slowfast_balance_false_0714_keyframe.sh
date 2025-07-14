28446ce283594fdf45dd5683c48118114f1d2e5b
caojiangxia <caojiangxia@kuaishou.com>
2025-07-14 17:22:36 +0800

    email=caojiangxia@kuaishou.com,time=20250714 17:22:36,script=examples/vlm/0.9.3/0714/keye_0.9.3_stage3_0.4.8_5e-5_slowfast_balance_false_0714_keyframe.sh,node=63,comment=version:0.9.3;model_size:8B:H800:512;Stage3:0.5.0,sharevit,15000seqlen,output=/mmu_mllm_hdd_2/zhouyang12/output1/Keye/0.9.1/Stage3_SlowFast/8b/slowfast_0712_keyframe_test, resume

--- examples/vlm/0.9.3/0714/keye_0.9.3_stage3_0.4.8_5e-5_slowfast_balance_false_0714_keyframe.sh
+++ examples/vlm/0.9.3/0714/keye_0.9.3_stage3_0.4.8_5e-5_slowfast_balance_false_0714_keyframe.sh
@@ -122,7 +122,7 @@ nohup mpirun --allow-run-as-root \
                 --allow_random_init_params 'mlp_AR.pre_norm.weight,mlp_AR.pre_norm.bias,mlp_AR.linear_1.weight,mlp_AR.linear_1.bias,mlp_AR.linear_2.weight,mlp_AR.linear_2.bias,visual_fast.vision_model.embeddings.packing_position_embedding.weight,fast_mlp_AR.pre_norm.weight,fast_mlp_AR.pre_norm.bias,fast_mlp_AR.linear_1.weight,fast_mlp_AR.linear_1.bias,fast_mlp_AR.linear_2.weight,fast_mlp_AR.linear_2.bias' \
                 --monitor_datasource_loss \
                 --monitor_datasource_cnt \
-                --max_length 80000 \
+                --max_length 40000 \
                 --learning_rate 5e-5 \
                 --vision_learning_rate 5e-6 \
                 --min_lr 5e-5 \
@@ -135,6 +135,9 @@ nohup mpirun --allow-run-as-root \
                 --use_flash_attention_2 \
                 --logging_per_step 20 \
                 --fp32_weight \
+
+
+                
                 --seed 19260817 \
                 --enable_gradient_checkpointing \
                 --merge_checkpoint \
