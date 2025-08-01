8b98a222ccff1621d12d2460218ffef4405bde80
caojiangxia <caojiangxia@kuaishou.com>
2025-08-01 20:46:27 +0800

    email=caojiangxia@kuaishou.com,time=20250801 20:46:27,script=examples/vlm/0.9.3/0801/sp8.sh,node=80,comment=从53Kstage2初始化，sp1，没有视频,output=/mmu_mllm_hdd_2/zhouyang12/output1/Keye/0.9.3/Stage3/8b/0801/sp8, resume

--- examples/vlm/0.9.3/0801/sp8.sh
+++ examples/vlm/0.9.3/0801/sp8.sh
@@ -15,12 +15,11 @@ fi
 sed 's/=1/=8/g' /etc/mpi/hostfile > /etc/mpi/hostfile_seq
 
 # MODEL_DIR=/llm_reco_ssd/luoxinchen/output/RecoVLM/Qwen2-VL-7B-stage1-v0.0.36/global_step90000-hf
-MODEL_DIR=/mmu_mllm_hdd_2/zhouyang12/models/Keye-8B-demo_hf_vit_rope_slowfast_0714_sp1
+MODEL_DIR=/mmu_mllm_hdd_2/zhouyang12/models/Keye-8B-demo_hf_vit_rope_slowfast_0714_2w_vtoken
 #OUTPUT_DIR=/llm_reco/maosiyang/train_out/0.9.1/keye_2B_stage1/
 OUTPUT_DIR=/mmu_mllm_hdd_2/zhouyang12/output1/Keye/0.9.3/Stage3/8b/0801/sp8
 
 mkdir -p $OUTPUT_DIR
-8sp
 mkdir -p /tmp/_wids_cache
 
 nnode=$(wc -l < /etc/mpi/hostfile_seq)
