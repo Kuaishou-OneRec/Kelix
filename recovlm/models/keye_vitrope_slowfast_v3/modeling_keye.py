2890a7c0379cdbfc6a78b6c40b21e86b89e2b0a8
caojiangxia <caojiangxia@kuaishou.com>
2025-08-06 17:24:43 +0800

    email=caojiangxia@kuaishou.com,time=20250806 17:24:43,script=examples/vlm/0.9.7/1d_vs_3d_rope/rope_1d_thw_0.3.1014.sh,node=1,comment=从53Kstage2初始化，sp1，没有视频,output=/mmu_mllm_hdd_2/zhouyang12/output1/Keye/0.9.7/Stage3/8b/1d_vs_3d_rope/rope1d_thw_0.3.1014, resume

--- recovlm/models/keye_vitrope_slowfast_v3/modeling_keye.py
+++ recovlm/models/keye_vitrope_slowfast_v3/modeling_keye.py
@@ -3433,7 +3433,7 @@ class KeyeForConditionalGeneration(Qwen3PreTrainedModel, GenerationMixin):
         #         "position_ids.pth"
         #     )
 
-        if dist.get_rank() == 0: torch.save(pos_ids, "pos_ids.pth"); exit()
+        if dist.get_rank() == 0: torch.save(position_ids, "position_ids.pth"); exit()
 
         position_ids_ = position_ids + 0
         print("position_ids_position_ids_", position_ids_)
