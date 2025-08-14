e101264807191f9276a69832842e13e171ab192d
caojiangxia <caojiangxia@kuaishou.com>
2025-08-14 14:10:39 +0800

    email=caojiangxia@kuaishou.com,time=20250814 14:10:39,script=examples/vlm/0.9.7/1d_vs_3d_rope_v2/rope_1d_stage1.sh,node=8,comment=从53Kstage2初始化，sp1，没有视频,output=/mmu_mllm_hdd_2/zhouyang12/output1/Keye/0.9.7/Stage1/8b/1d_vs_3d_rope_v2/rope1d_0814, resume

--- examples/vlm/0.9.7/1d_vs_3d_rope_v2/rope_1d_stage1.sh
+++ examples/vlm/0.9.7/1d_vs_3d_rope_v2/rope_1d_stage1.sh
@@ -125,7 +125,7 @@ nohup mpirun --allow-run-as-root \
                 --allow_random_init_params 'mlp_AR.pre_norm.weight,mlp_AR.pre_norm.bias,mlp_AR.linear_1.weight,mlp_AR.linear_1.bias,mlp_AR.linear_2.weight,mlp_AR.linear_2.bias,visual_fast.vision_model.embeddings.packing_position_embedding.weight,fast_mlp_AR.pre_norm.weight,fast_mlp_AR.pre_norm.bias,fast_mlp_AR.linear_1.weight,fast_mlp_AR.linear_1.bias,fast_mlp_AR.linear_2.weight,fast_mlp_AR.linear_2.bias' \
                 --monitor_datasource_loss \
                 --monitor_datasource_cnt \
-                --max_length 16500 \
+                --max_length 17000 \
                 --learning_rate 1e-3 \
                 --vision_learning_rate 2e-6 \
                 --min_lr 0.0099 \
