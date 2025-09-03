025a4e751ce1324c5c8d76acb6e0629dbe25f561
yangzhuoran <yangzhuoran@kuaishou.com>
2025-09-03 20:19:59 +0800

    email=yangzhuoran@kuaishou.com,time=20250903 20:19:59,script=examples/vlm/0.9.7/1d_vs_3d_rope_v2/rope_3d.sh,node=2,comment=从53Kstage2初始化，sp1，没有视频,output=/llm_reco_ssd/yangzhuoran/code/outputs/navit-tar/tatok_class,resume

--- recovlm/models/keye_vitrope_slowfast_tatok/tok/utils.py
+++ recovlm/models/keye_vitrope_slowfast_tatok/tok/utils.py
@@ -5,8 +5,11 @@ import torch.nn as nn
 class ScalingLayer(nn.Module):
     def __init__(self, mean=[0.5, 0.5, 0.5], std=[0.5, 0.5, 0.5]):
         super().__init__()
-        self.register_buffer('shift', torch.Tensor(mean)[None, :, None, None])
-        self.register_buffer('scale', torch.Tensor(std)[None, :, None, None])
+        # self.register_buffer('shift', torch.Tensor(mean)[None, :, None, None])
+        # self.register_buffer('scale', torch.Tensor(std)[None, :, None, None])
+
+        self.shift = nn.Parameter(torch.Tensor(mean)[None, :, None, None])
+        self.scale = nn.Parameter(torch.Tensor(std)[None, :, None, None])
 
     def forward(self, inp):
         return (inp - self.shift) / self.scale
