5239b22e6e90c2f4e3d09e9bb64fa12b1eb00d91
zhouyang12 <zhouyang12@kuaishou.com>
2025-10-06 17:52:46 +0800

    email=zhouyang12@kuaishou.com,time=20251006 17:52:46,script=examples/vq/run_vq.sh,node=1,comment=vq_test,output=/mmu_mllm_hdd_2/zhouyang12/output/Keye/vq_test/8B/, resume

--- examples/vq/run_vq.sh
+++ examples/vq/run_vq.sh
@@ -17,6 +17,7 @@ sed 's/=1/=8/g' /etc/mpi/hostfile > /etc/mpi/hostfile_seq
 # MODEL_DIR=/llm_reco_ssd/luoxinchen/output/RecoVLM/Qwen2-VL-7B-stage1-v0.0.36/global_step90000-hf
 MODEL_DIR=/llm_reco_ssd/zhouyang12/models/KeyeImageTokenizer_init/
 OUTPUT_DIR=/mmu_mllm_hdd_2/zhouyang12/output/Keye/vq_test/8B/
+export LD_LIBRARY_PATH=/usr/local/cuda/lib64:$LD_LIBRARY_PATH
 
 rm -rf $OUTPUT_DIR
 mkdir -p $OUTPUT_DIR
