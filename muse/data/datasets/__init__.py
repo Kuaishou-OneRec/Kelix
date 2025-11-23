ac9749e45611137e21c4711b9df3a6ed2d9a875b
zhouyang <zhouyang12@kuaishou.com>
2025-11-23 10:49:57 +0800

    fix

--- muse/data/datasets/__init__.py
+++ muse/data/datasets/__init__.py
@@ -1,13 +1,8 @@
-from .base import DistributedDataset, ParquetDataset, get_worker_info
-from .recoverable import RecoverableDistributedDataset
-from .factory import create_dataset, get_recoverable_dataset
+from .base import DistributedDataset
+from .text import TextDataset
 
 # Backward compatibility - expose all classes
 __all__ = [
-    'DistributedDataset', 
-    'ParquetDataset',
-    'RecoverableDistributedDataset', 
-    'create_dataset',
-    'get_recoverable_dataset',
-    'get_worker_info'
+    'DistributedDataset',
+    'TextDataset',
 ]
