52eadb097a6722a029256b38eef6723857585b5a
maosiyang <you@kuaishou.com>
2025-12-03 23:32:38 +0800

    add siglip

--- muse/models/Siglip/modeling.py
+++ muse/models/Siglip/modeling.py
@@ -204,7 +204,7 @@ class SiglipVisionEmbeddings(nn.Module):
         spatial_shapes = []
         for thw_tuple in image_grid_thw:
             spatial_shapes.append((thw_tuple[1],thw_tuple[2]))
-        spatial_shapes = spatial_shapes.concat(dim=0)
+        spatial_shapes = torch.tensor(spatial_shapes, dtype=torch.long, device=pixel_values.device)
 
         # Get positional resized and padded positional embeddings
         positional_embeddings = self.position_embedding.weight.reshape(
