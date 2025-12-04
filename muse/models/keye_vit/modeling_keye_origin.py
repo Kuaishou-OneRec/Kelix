f97414c73afedf91c17fccd7ec718fc25daad59a
maosiyang <you@kuaishou.com>
2025-12-04 20:20:23 +0800

    add siglip

--- muse/models/keye_vit/modeling_keye_origin.py
+++ muse/models/keye_vit/modeling_keye_origin.py
@@ -850,7 +850,7 @@ class SiglipMLP(nn.Module):
     def __init__(self, config):
         super().__init__()
         self.config = config
-        self.activation_fn = ACT2FN[config.hidden_act]
+        self.activation_fn = ACT2FN['silu']
         self.fc1 = nn.Linear(config.hidden_size, config.intermediate_size)
         self.fc2 = nn.Linear(config.intermediate_size, config.hidden_size)
 
