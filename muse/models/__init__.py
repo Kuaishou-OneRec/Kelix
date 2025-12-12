17837225f68181d47bb89ff1f33621f8412d0856
maosiyang <you@kuaishou.com>
2025-12-12 14:17:27 +0800

    add siglip

--- muse/models/__init__.py
+++ muse/models/__init__.py
@@ -78,7 +78,6 @@ def list_models() -> List[str]:
 
 # Import model modules to trigger registration
 from muse.models import qwen3  # noqa: E402, F401
-# Note: Siglip is imported directly via muse.models.Siglip to avoid circular imports
 
 # Export public API
 __all__ = [
