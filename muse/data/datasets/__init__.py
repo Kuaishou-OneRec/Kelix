228213656c70ab0116bdc73e4fafe6c64055691a
zhouyang12 <zhouyang12@kuaishou.com>
2025-12-12 14:37:48 +0800

    email=zhouyang12@kuaishou.com,time=20251212 14:37:48,script=examples/sana/gemma/run_train_multi_scale.sh,node=1,comment=sana_t2i_ketu,output=/mmu_mllm_hdd_2/zhouyang12/output/MuseV2/sana/t2i_multi_scale, resume

--- muse/data/datasets/__init__.py
+++ muse/data/datasets/__init__.py
@@ -1,10 +1,11 @@
 from .base import DistributedDataset
 from .text import TextDataset
-from .image import Text2ImageDataset
+from .image import Text2ImageDataset, Text2ImageMultiScaleDatasetWrapper
 
 # Backward compatibility - expose all classes
 __all__ = [
     'DistributedDataset',
     'TextDataset',
     'Text2ImageDataset',
+    'Text2ImageMultiScaleDatasetWrapper'
 ]
