7d585e32ee948c1091b3b413e4db18cf5d798a12
zhouyang <zhouyang12@kuaishou.com>
2025-12-14 18:06:09 +0800

    add visualization

--- examples/sana/gemma/run_train_debug.sh
+++ examples/sana/gemma/run_train_debug.sh
@@ -139,6 +139,7 @@ nohup mpirun --allow-run-as-root \
                 --fp32-weight \
                 --fp32-reduce \
                 --visualize \
+                --eval-sampling-steps 30 \
                 --seed 19260817 \
                 --enable-gradient-checkpointing \
                 --prefetch-params-in-forward \
