c822a3e40bb31eccc48f1b8ab8600fb248524c15
lingzhixin <chuchenglong@kuaishou.com>
2025-12-18 19:22:55 +0800

    c

--- muse/models/keye_ar/modeling.py
+++ muse/models/keye_ar/modeling.py
@@ -57,8 +57,11 @@ class UnifiedTokenEmbedding(nn.Module):
 
     def _embedding_aggregation(self, extended_tokens, embeddings):
         import IPython
-        print(extended_tokens.shape, embeddings.shape)
-        IPython.embed()
+        if embeddings.size(2) == extended_tokens.size(2) - 1:
+            embeddings = torch.nn.functional.pad(embeddings, (0, 0, 0, 1), value=0)
+
+        #print(extended_tokens.shape, embeddings.shape)
+        #IPython.embed()
 
         # 获取q_eos token id
         q_eos_token_id = self.q_eos_token
