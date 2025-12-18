e478d2e891bbedb161daeb406d4ceeff1808b46a
lingzhixin <chuchenglong@kuaishou.com>
2025-12-18 19:53:53 +0800

    c

--- muse/models/keye_ar/modeling.py
+++ muse/models/keye_ar/modeling.py
@@ -316,7 +316,7 @@ class UnifiedQwen3Model(Qwen3Model):
             num_heads=self.model.num_heads,
             head_dim=self.model.head_dim,
             norm=self.model.norm,
-            output=self.model.output,
+            output=nn.Linear(qwen_config.vocab_size + token_decoder_config.codebook_size, qwen_config.embed_dim),
             token_head=token_head
         )
 
