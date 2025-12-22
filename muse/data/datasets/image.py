88fbd690306253f737e60ba44c5d6949b0243fd9
lingzhixin <chuchenglong@kuaishou.com>
2025-12-23 00:27:57 +0800

    c

--- muse/data/datasets/image.py
+++ muse/data/datasets/image.py
@@ -1055,7 +1055,7 @@ class Chat2ImageDataset(Token2ImageDataset):
             condition_image = image
 
         # Get message from sample for chat template processing
-        messages = sample.get("message")
+        messages = sample["meessage"]
 
         # Apply chat template using the message from sample
         text = self.processor.apply_chat_template(
@@ -1113,8 +1113,10 @@ class Chat2ImageDataset(Token2ImageDataset):
             if height is not None and width is not None:
                 pair["height"] = height
                 pair["width"] = width
-
+            
+            pair["message"] = sample["messages"]
             print(f"sample={sample}\npair={pair}")
+            # pair={'image': '/mmu_mllm_hdd_2/zhouyang12/media/images/cc/5a/ee/fd/53/mmu-vcg-data-bd6c6e695e21adeba3038ad5d0a327d1.jpg', 'text': 'The image features a gray leather handbag with a textured surface and two handles, accompanied by a strap with red and navy stripes. The handbag is positioned against a plain white background, highlighting its design and details.', 'height': 2000, 'width': 1128}
             return pair
         return None
 
