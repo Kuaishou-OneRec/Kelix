5b22696bb9d01e34ebd8f54213f01a66549c179e
lingzhixin <chuchenglong@kuaishou.com>
2025-12-25 04:28:22 +0800

    c

--- muse/models/keye_ar/verify_logits_consistency_v2_clean.py
+++ muse/models/keye_ar/verify_logits_consistency_v2_clean.py
@@ -295,8 +295,9 @@ def main():
     
     output_logit_file = "/mmu_mllm_hdd_2/lingzhixin/model_verification/muse_v2/verify_logits_consistency_v2/keye_conditional_generation.pt"
     keye_conditional_logits = torch.load(output_logit_file).reshape(keye_ar_logits.shape)
-
-    assert torch.allclose(keye_conditional_logits, keye_ar_logits)
+    # import IPython
+    # IPython.embed()
+    assert torch.allclose(keye_conditional_logits.to(keye_ar_logits), keye_ar_logits)
     
     
 
