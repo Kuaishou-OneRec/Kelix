c211a17931d2c8d572fbead21935edefcb897728
lingzhixin <chuchenglong@kuaishou.com>
2025-12-25 15:13:52 +0800

    c

--- examples/sana/ar_dit/run_ar_dit_lzx_4096_v2_512im.sh
+++ examples/sana/ar_dit/run_ar_dit_lzx_4096_v2_512im.sh
@@ -122,7 +122,7 @@ nohup mpirun --allow-run-as-root \
                 --visualize-parquet-path $VISUAL_PARQUET_PATH \
                 --visualize-per-step 100 \
                 --keye-ar-dir $KEYE_AR_DIR \
-                --num-vis-images 12 \
+                --num-vis-images 14 \
                 --model-dir $MODEL_DIR \
                 --vae-dir $VAE_DIR \
                 --max-condition-length 324 \
