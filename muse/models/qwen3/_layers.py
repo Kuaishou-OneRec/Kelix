ffed8d87759c606e59347b6bc9f1804250e96dce
lingzhixin <chuchenglong@kuaishou.com>
2025-12-25 19:52:08 +0800

    c

--- muse/models/qwen3/_layers.py
+++ muse/models/qwen3/_layers.py
@@ -253,6 +253,8 @@ class Qwen3Attention(nn.Module):
 
             # Update key-value cache
             if self.kv_cache is not None and self.cache_enabled:
+                import IPython
+                IPython.embed()
                 # KVCache内部存储格式是 [b, n_kv, max_seq_len, h_d]
                 # KVCache.update期望输入是 [b, s, n_kv, h_d]，但内部会按 [b, n_kv, s, h_d] 处理
                 # 所以我们需要先将输入从 [b, s_y, n_kv, h_d] 转换为 [b, n_kv, s_y, h_d] 再传入
