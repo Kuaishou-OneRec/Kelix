e75fb91c06cb240519213dde88774569dda911e5
lingzhixin <chuchenglong@kuaishou.com>
2026-01-05 13:33:30 +0800

    c

--- examples/sana/ae/inference/run_inference_auto_encoder_vlm_dcp.sh
+++ examples/sana/ae/inference/run_inference_auto_encoder_vlm_dcp.sh
@@ -2,7 +2,7 @@
 # 注意: max-condition-length要按照实际vlm输出的token数来，因为计算rope时要获取实际的height和width，如果max-condition-length过大，会导致rope的计算与实际token数有diff
 torchrun --nproc_per_node=8 recipes/sana/inference_auto_encoder_vlm.py \
         --model-dir /llm_reco_ssd/zhouyang12/models/muse/Sana_1600M_1024px/ \
-        --dcp-dir /mmu_mllm_hdd_2/zhouyang12/output/MuseV2/sana_v2/multi_scale/exp3.22/ \
+        --dcp-ckpt-dir /mmu_mllm_hdd_2/zhouyang12/output/MuseV2/sana_v2/multi_scale/exp3.22/ \
         --dcp-tag global_step100000 \
         --vae-dir /llm_reco_ssd/zhouyang12/models/SANA1.5_1.6B_1024px_diffusers/vae/ \
         --image-tokenizer-dir /llm_reco_ssd/zhouyang12/models/muse/KeyeTokenizer/ \
