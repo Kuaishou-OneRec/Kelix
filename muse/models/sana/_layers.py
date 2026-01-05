3ffd30c2e55362dcece520e5bbef7e306c446ad2
lingzhixin <chuchenglong@kuaishou.com>
2026-01-05 16:14:46 +0800

    c

--- muse/models/sana/_layers.py
+++ muse/models/sana/_layers.py
@@ -438,7 +438,6 @@ class MultiHeadCrossAttention(nn.Module):
         k = self.k_norm(k).view(B, -1, self.num_heads, self.head_dim)
         v = v.view(B, -1, self.num_heads, self.head_dim)
         
-        print(f"self.pos_embeddings={self.pos_embeddings}, x_input_pos is not None={x_input_pos is not None}, cond_input_pos is not None={cond_input_pos is not None}")
         # Apply 2D RoPE using Roraty2DPositionalEmbeddings
         if self.pos_embeddings is not None and x_input_pos is not None:
             q = self.pos_embeddings(q, input_pos=x_input_pos)
