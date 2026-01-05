dcba83d3fb787a06a087c69f37845cceac891535
lingzhixin <chuchenglong@kuaishou.com>
2026-01-05 12:02:37 +0800

    c

--- examples/sana/ar_dit/inference/infer_and_eval.sh
+++ examples/sana/ar_dit/inference/infer_and_eval.sh
@@ -1,5 +1,5 @@
 DCP_CKPT_DIR=/mmu_mllm_hdd_2/lingzhixin/output/MuseV2/sana/ar_dit/exp18_ar_dit_multiscale_324tokens_2e-5/ \
-DCP_TAG=global_step10000 \
+DCP_TAG=global_step52000 \
 bash examples/sana/ar_dit/inference/mpi_infer_custom.sh
 work_dir=${DCP_CKPT_DIR}/${DCP_TAG}/inference/GenEval/outputs/ulmeval/aggresults/
 source /mmu_mllm_hdd_2/chuchenglong/miniconda3/bin/activate 
