2e3bfc028699af9a98185e14cd74b631631fea23
lingzhixin <lingzhixin@kuaishou.com>
2026-01-06 02:30:34 +0000

    c

--- examples/sana/ar_dit/inference/run_auto_monitor.sh
+++ examples/sana/ar_dit/inference/run_auto_monitor.sh
@@ -14,6 +14,11 @@ DATASET_CONFIG="examples/sana/ar_dit/inference/run_ar_dit_lzx_4096_v2_1024im_mul
 KEYE_AR_DIR="/mmu_mllm_hdd_2/zhouyang12/output/Keye/vqar_11.7/run_8b_vis_stage3.29_1e-4/step18000/global_step18000/muse_converted"
 INFERENCE_SCRIPT="examples/sana/ar_dit/inference/mpi_infer_custom.sh"
 
+
+DCP_CKPT_DIR="/mmu_mllm_hdd_2/lingzhixin/output/MuseV2/sana/ar_dit/exp21_ar_dit_324tokens_1e-4_reproduce"
+DATASET_CONFIG="examples/sana/ar_dit/exp21_ar_dit_324tokens_1e-4_reproduce.json"
+INFERENCE_SCRIPT="examples/sana/ar_dit/inference/mpi_infer_custom_v2.sh"
+
 # Run the Python script with all parameters
 PYTHONPATH=. \
 python3 -u examples/keye_ar/auto_infer_eval.py \
