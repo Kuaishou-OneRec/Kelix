a20e615a84ab51559f2527fefb858f834bc2a308
lingzhixin <chuchenglong@kuaishou.com>
2026-01-06 15:30:20 +0800

    c

--- examples/sana/ar_dit/inference/infer_and_eval_v2.sh
+++ examples/sana/ar_dit/inference/infer_and_eval_v2.sh
@@ -1,8 +1,7 @@
-DCP_CKPT_DIR=/mmu_mllm_hdd_2/lingzhixin/output/MuseV2/sana/ar_dit/exp18_ar_dit_multiscale_324tokens_2e-5/
-DCP_TAG=global_step52000
 
-DCP_CKPT_DIR=${DCP_CKPT_DIR} DCP_TAG=${DCP_TAG}  bash examples/sana/ar_dit/inference/mpi_infer_custom.sh
-work_dir=${DCP_CKPT_DIR}/${DCP_TAG}/inference/GenEval/outputs/ulmeval/aggresults/
+MODEL_DIR=/mmu_mllm_hdd_2/zangdunju/output2/RecoVLM/DiTSFT/batch6_324_1024_more_data/global_step80000/muse_converted/
+MODEL_DIR=${MODEL_DIR} bash examples/sana/ar_dit/inference/mpi_infer_custom.sh
+work_dir=${MODEL_DIR}/inference/GenEval/outputs/ulmeval/aggresults/
 source /mmu_mllm_hdd_2/chuchenglong/miniconda3/bin/activate 
 conda activate ulmevalkit2
 cd /llm_reco/lingzhixin/dit_eval_lzx/ULMEvalKit
