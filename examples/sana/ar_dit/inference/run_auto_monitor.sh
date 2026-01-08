a64150f631086976cea754f0c38f9c86863b692a
lingzhixin <lingzhixin@kuaishou.com>
2026-01-08 10:00:46 +0000

    c

--- examples/sana/ar_dit/inference/run_auto_monitor.sh
+++ examples/sana/ar_dit/inference/run_auto_monitor.sh
@@ -30,12 +30,14 @@ INFERENCE_SCRIPT="examples/sana/ar_dit/inference/mpi_infer_custom_v2.sh"
 
 DCP_CKPT_DIR="/mmu_mllm_hdd_2/lingzhixin/output/MuseV2/sana/ar_dit/exp31_ar_dit_324tokens_1e-4_cond_special/"
 MAX_CONDITION_LENGTH=658
+INFERENCE_SCRIPT="examples/sana/ar_dit/inference/mpi_infer_custom_cond_spe.sh"
 MODEL_CONFIG_OVERRIDES="model_max_length=658"
 log_file=${DCP_CKPT_DIR}/auto_monitor.log
 echo "log_file=${log_file}"
 
 # Run the Python script with all parameters
 PYTHONPATH=. \
+INFERENCE_SCRIPT=${INFERENCE_SCRIPT} \
 MODEL_CONFIG_OVERRIDES=${MODEL_CONFIG_OVERRIDES} \
 MAX_CONDITION_LENGTH=${MAX_CONDITION_LENGTH} \
 nohup python3 -u examples/keye_ar/auto_infer_eval.py \
