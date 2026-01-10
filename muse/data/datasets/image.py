941c691515fe085a51df357096811d921b6c9383
lingzhixin <chuchenglong@kuaishou.com>
2026-01-10 11:43:41 +0800

    c

--- muse/data/datasets/image.py
+++ muse/data/datasets/image.py
@@ -1340,7 +1340,7 @@ class Chat2ImageDataset(Token2ImageDataset):
         # Here is the real process of batch.
         result = {}
         batch = [self._process_pair(sample) for sample in batch]
-
+        print(f"batch none check", [x is None for x in batch])
         # Concatenate pixel_values: [s, d] -> [S, d] where S is sum of all s
         result["pixel_values"] = torch.concat([s["pixel_values"] for s in batch], dim=0)
         result["image_grid_thw"] = torch.concat([s["image_grid_thw"] for s in batch], dim=0)
