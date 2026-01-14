2b98b2adb2d63a247c6e638b3f88cce75c135706
lingzhixin <chuchenglong@kuaishou.com>
2026-01-14 09:32:49 +0800

    c

--- examples/sana/ar_dit/exp8x/exp81.2_large_dit.sh
+++ examples/sana/ar_dit/exp8x/exp81.2_large_dit.sh
@@ -146,7 +146,7 @@ nohup mpirun --allow-run-as-root \
                 --allow-random-init-params "diffusion_connector.0.weight,diffusion_connector.0.bias,diffusion_connector.2.weight,diffusion_connector.2.bias,diffusion_connector.3.weight" \
                 --skip-load-params "y_embedder.y_embedding" \
                 --dataset-config examples/sana/ar_dit/exp8x/exp82.1_324_token.json \
-                --resolution-budgets "1024:20" \
+                --resolution-budgets "1024:12" \
                 --learning-rate 2e-4 \
                 --min-lr 1e-4 \
                 --num-decay-steps 6000 \
@@ -154,7 +154,7 @@ nohup mpirun --allow-run-as-root \
                 --image-size 1024 \
                 --beta1 0.9 \
                 --beta2 0.95 \
-                --batch-size 20 \
+                --batch-size 12 \
                 --lr-scheduler-type cosine_v2 \
                 --num-warmup-steps 100 \
                 --num-training-steps 300000 \
