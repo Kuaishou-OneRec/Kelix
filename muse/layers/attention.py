867f3ce79a51f02a797c1b1f588e87b83fcc9b58
lingzhixin <chuchenglong@kuaishou.com>
2026-01-15 02:40:53 +0800

    c

--- muse/layers/attention.py
+++ muse/layers/attention.py
@@ -251,7 +251,7 @@ class MultiHeadAttention(nn.Module):
 
         # Apply positional embeddings
         if self.pos_embeddings is not None:
-            print("pos_embeddings in attention", q.shape, input_pos.shape, input_pos)
+            print("pos_embeddings in attention", q.shape, input_pos.shape, input_pos.cpu().tolist())
             q = self.pos_embeddings(q, input_pos=input_pos)
 
         # Normalize q
