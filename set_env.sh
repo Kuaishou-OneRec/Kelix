ae2dac2f1349dfced9dacbb2d9475292d0635453
lingzhixin <lingzhixin@kuaishou.com>
2026-01-15 15:49:25 +0000

    c

--- set_env.sh
+++ set_env.sh
@@ -31,6 +31,7 @@ echo "Loaded environment variables from ${ENV_FILE}:"
 cat "${ENV_FILE}"
 
 nohup rm -rf hs_err_pid*.log &
+<<<<<<< HEAD
 
 # if there is no /home/hadoop, run the command below
 if [ ! -d "/home/hadoop" ]; then
@@ -39,6 +40,12 @@ if [ ! -d "/home/hadoop" ]; then
 fi
 
 mpirun --allow-run-as-root --hostfile /etc/mpi/hostfile --pernode bash -c "/opt/conda/envs/py312/bin/pip3 install keye_vl_utils"
+=======
+#mpirun --allow-run-as-root --hostfile /etc/mpi/hostfile --pernode bash -c "cp -r /mmu_mllm_hdd_2/lingzhixin/envs/hadoop/ /home/hadoop" 
+#mpirun --allow-run-as-root --hostfile /etc/mpi/hostfile --pernode bash -c "/opt/conda/envs/py312/bin/pip3 install keye_vl_utils"
+# sortedcontainers
+#mpirun --allow-run-as-root --hostfile /etc/mpi/hostfile --pernode bash -c "/opt/conda/envs/py312/bin/pip3 install sortedcontainers"
+>>>>>>> 00ef33532 (c)
 # registry.corp.kuaishou.com/kml-supercomputing-project/v1.6_vllm0.7.3_ray_torch2.5.1_cuda11.8.0_python3.10.12_hadoop_xray:v1
 #mpirun --allow-run-as-root --hostfile /etc/mpi/hostfile -x http_proxy=http://oversea-squid1.jp.txyun:11080 -x https_proxy=http://oversea-squid1.jp.txyun:11080 --pernode bash -c  "apt-get install numactl"
 #mpirun --allow-run-as-root --hostfile /etc/mpi/hostfile --pernode bash -c "pip3 install easydict"
