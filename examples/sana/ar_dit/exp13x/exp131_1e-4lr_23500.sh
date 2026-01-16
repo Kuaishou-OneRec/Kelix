d95665e4ac609ca7d534307be3b7c2fc7ef29c72
lingzhixin <chuchenglong@kuaishou.com>
2026-01-17 01:39:59 +0800

    c

--- examples/sana/ar_dit/exp13x/exp131_1e-4lr_23500.sh
+++ examples/sana/ar_dit/exp13x/exp131_1e-4lr_23500.sh
@@ -15,11 +15,18 @@ fi
 sed 's/=1/=8/g' /etc/mpi/hostfile > /etc/mpi/hostfile_seq
 script_name=$(basename "$0" .sh)
 
+
+
+PYTHONPATH=. python3 examples/keye_ar/convert_hf_checkpoint.py \
+        --hf-checkpoint-path /mmu_mllm_hdd_2/zhouyang12/output/Keye/vqar_11.9.1/v9.2_stage3_0.81_128u/step23500/global_step23500/converted/ \
+        --output-dir /mmu_mllm_hdd_2/zhouyang12/output/Keye/vqar_11.9.1/v9.2_stage3_0.81_128u/step23500/global_step23500/muse_converted/
+
+
 MODEL_DIR=/llm_reco_ssd/zhouyang12/models/muse/Sana_1600M_1024px-reproduce-0105/
 MODEL_CONFIG=/llm_reco_ssd/zhouyang12/models/muse/Sana_1600M_1024px-reproduce-0105/config.json
 VAE_DIR=/llm_reco_ssd/zhouyang12/models/SANA1.5_1.6B_1024px_diffusers/vae/
 # IMAGE_TOKENIZER_DIR=/llm_reco_ssd/zhouyang12/models/muse/KeyeTokenizer/
-KEYE_AR_DIR=/mmu_mllm_hdd_2/zhouyang12/output/Keye/vqar_11.9.1/v8_stage3_0.29/step18000/global_step18000/muse_converted
+KEYE_AR_DIR=/mmu_mllm_hdd_2/zhouyang12/output/Keye/vqar_11.9.1/v9.2_stage3_0.81_128u/step23500/global_step23500/muse_converted
 VISUALIZE_DIR=/llm_reco_ssd/zhouyang12/data/val_images/
 VISUAL_PARQUET_PATH=/mmu_mllm_hdd_2/lingzhixin/recovlm_data/muse_v2/vis/vis_data0110.parquet
 
@@ -145,22 +152,22 @@ nohup mpirun --allow-run-as-root \
                 --output-dir $OUTPUT_DIR \
                 --allow-random-init-params "diffusion_connector.0.weight,diffusion_connector.0.bias,diffusion_connector.2.weight,diffusion_connector.2.bias,diffusion_connector.3.weight" \
                 --skip-load-params "y_embedder.y_embedding" \
-                --dataset-config examples/sana/ar_dit/exp10x/exp103_ar_dit_324tokens_1e-4_baseline.json \
-                --resolution-budgets "1024:24" \
+                --dataset-config examples/sana/ar_dit/exp13x/exp130.json \
+                --resolution-budgets "1024:20" \
                 --learning-rate 1e-4 \
                 --min-lr 1e-4 \
-                --num-decay-steps 4000 \
+                --num-decay-steps 6000 \
                 --weight-decay 0.0 \
                 --image-size 1024 \
                 --beta1 0.9 \
                 --beta2 0.95 \
-                --batch-size 24 \
+                --batch-size 20 \
                 --lr-scheduler-type cosine_v2 \
                 --num-warmup-steps 100 \
                 --num-training-steps 300000 \
                 --model-config-overrides model_max_length=720 \
                 --condition-on-special-tokens \
-                --save-checkpoint-per-step 200 \
+                --save-checkpoint-per-step 1000 \
                 --logging-per-step 20 \
                 --clip-range 9999999 \
                 --fp32-weight \
