a4c911dbccd871798ad69909c3acc1c2d8387ce8
lingzhixin <chuchenglong@kuaishou.com>
2026-01-18 15:05:16 +0800

    c

--- examples/sana/ar_dit/exp13x/exp146_monitor.sh
+++ examples/sana/ar_dit/exp13x/exp146_monitor.sh
@@ -24,6 +24,7 @@ MODEL_CONFIG_OVERRIDES="model_max_length=720"
 log_file=${DCP_CKPT_DIR}/auto_monitor.log
 echo "log_file=${log_file}"
 
+ln -s /mmu_mllm_hdd_2/lingzhixin/output/MuseV2/ar_dit/exp13x/exp145_0116sftv1_1e-4lr_pt/token_cache ${DCP_CKPT_DIR}
 
 # Run the Python script with all parameters
 PYTHONPATH=. \
