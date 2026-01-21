bea2c23988299705dc9879923de32fe48431712a
lingzhixin <chuchenglong@kuaishou.com>
2026-01-22 04:21:55 +0800

    c

--- examples/keye_ar/serve_dit/while_demo_client.sh
+++ examples/keye_ar/serve_dit/while_demo_client.sh
@@ -21,11 +21,4 @@ while true; do
   n=$((n+1))
   echo "\n===== [$n] $(date '+%Y-%m-%d %H:%M:%S') ====="
   bash "$CLIENT_SH"
-
-  if [[ -n "$LOOPS" && "$n" -ge "$LOOPS" ]]; then
-    echo "Reached loops=$LOOPS, exiting."
-    break
-  fi
-
-  sleep "$SLEEP_SECS"
 done
