8ea6b7eb3b6b209e63b93d36ace0379e5aac9568
lingzhixin <chuchenglong@kuaishou.com>
2026-01-24 11:54:12 +0800

    c

--- examples/sana/ar_dit/exp4x/exp67_mpi_infer_custom_0124_sft_exp_step500.sh
+++ examples/sana/ar_dit/exp4x/exp67_mpi_infer_custom_0124_sft_exp_step500.sh
@@ -42,7 +42,7 @@ MAX_CONDITION_LENGTH="${MAX_CONDITION_LENGTH:-720}"
 IMAGE_SIZE="${IMAGE_SIZE:-1024}"
 SEED="${SEED:-42}"
 INITIALIZE_DIST="${INITIALIZE_DIST:-true}"  # initialize a local single-process dist group
-MODEL_CONFIG_OVERRIDES="${MODEL_CONFIG_OVERRIDES:-model_max_length=720}"/mmu_mllm_hdd_2/lingzhixin/output/MuseV2/ar_dit/exp4x/exp42_ar_dit_324tokens_1e-4_cond_special/
+MODEL_CONFIG_OVERRIDES="${MODEL_CONFIG_OVERRIDES:-model_max_length=720}
 
 # DCP_CKPT_DIR="${DCP_CKPT_DIR:-/mmu_mllm_hdd_2/lingzhixin/output/MuseV2/sana/ar_dit/exp21_ar_dit_324tokens_1e-4_reproduce/}"
 # DCP_TAG="${DCP_TAG:-global_step12000}"
@@ -54,7 +54,7 @@ MODEL_CONFIG_OVERRIDES="${MODEL_CONFIG_OVERRIDES:-model_max_length=720}"/mmu_mll
 # DCP_CKPT_DIR="${DCP_CKPT_DIR:-/mmu_mllm_hdd_2/lingzhixin/output/MuseV2/ar_dit/exp13x/exp144_1e-4lr_23500_pt/}"
 # DCP_TAG="${DCP_TAG:-global_step6000}"
 
-DCP_CKPT_DIR=/mmu_mllm_hdd_2/lingzhixin/output/MuseV2/ar_dit/exp4x/exp67_ar_dit_324tokens_1e-4_sft/
+DCP_CKPT_DIR="/mmu_mllm_hdd_2/lingzhixin/output/MuseV2/ar_dit/exp4x/exp42_ar_dit_324tokens_1e-4_cond_special/"
 DCP_TAG=global_step500
 
 TEACHER_FORCING="${TEACHER_FORCING:-0}"
