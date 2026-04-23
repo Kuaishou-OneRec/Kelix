2242a4a51e3c62a7137fdf5dacb772bb8f9d3edd
lingzhixin <chuchenglong@kuaishou.com>
2026-04-23 23:34:13 +0800

    c

--- set_env.sh
+++ set_env.sh
@@ -37,11 +37,20 @@ nohup rm -rf hs_err_pid*.log &
 #     mpirun --allow-run-as-root --hostfile /etc/mpi/hostfile --pernode bash -c "cp -r /mmu_mllm_hdd_2/lingzhixin/envs/hadoop/ /home/hadoop" 
 # && echo "Directory copied successfully." || echo "Failed to copy directory."
 # fi
+CUSTOM_PIP="/opt/conda/envs/py312/bin/pip3"
 
-mpirun --allow-run-as-root --hostfile /etc/mpi/hostfile --pernode bash -c "/opt/conda/envs/py312/bin/pip3 install keye_vl_utils; /opt/conda/envs/py312/bin/pip3 install sortedcontainers"
+# 判断路径是否存在且可执行，赋值PIP_CMD
+if [ -x "$CUSTOM_PIP" ]; then
+    PIP_CMD="$CUSTOM_PIP"
+else
+    PIP_CMD="pip3"
+fi
+
+
+mpirun --allow-run-as-root --hostfile /etc/mpi/hostfile --pernode bash -c "${PIP_CMD} install keye_vl_utils; ${PIP_CMD} install sortedcontainers"
 # registry.corp.kuaishou.com/kml-supercomputing-project/v1.6_vllm0.7.3_ray_torch2.5.1_cuda11.8.0_python3.10.12_hadoop_xray:v1
 #mpirun --allow-run-as-root --hostfile /etc/mpi/hostfile -x http_proxy=http://oversea-squid1.jp.txyun:11080 -x https_proxy=http://oversea-squid1.jp.txyun:11080 --pernode bash -c  "apt-get install numactl"
-mpirun --allow-run-as-root --hostfile /etc/mpi/hostfile --pernode bash -c "/opt/conda/envs/py312/bin/pip3  install easydict"
+mpirun --allow-run-as-root --hostfile /etc/mpi/hostfile --pernode bash -c "${PIP_CMD}  install easydict"
 # mpirun --allow-run-as-root --hostfile /etc/mpi/hostfile --pernode bash -c "pip3 install transformers==4.49"
 # mpirun --allow-run-as-root --hostfile /etc/mpi/hostfile --pernode bash -c "pip3 install --upgrade torchao"
 # mpirun --allow-run-as-root --hostfile /etc/mpi/hostfile --pernode bash -c "pip3 install timm==1.0.15"
